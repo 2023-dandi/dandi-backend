064e558f37c5a92dce05cab9979632adc0ef088e
Chris <bjuuuu98@gmail.com>
2023-01-04 16:15:15 +0900

    set up project

---
+++ src/main/java/dandi/dandi/DandiApplication.java
@@ -0,0 +1,12 @@
+package dandi.dandi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DandiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DandiApplication.class, args);
+	}
+}
