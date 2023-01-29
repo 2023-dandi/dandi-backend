b39e1cc28c577b6dfc29b01c53c2b08dd37277b5
Chris <bjuuuu98@gmail.com>
2023-01-29 15:21:59 +0900

    feat: idToken에서 header를 추출하는 기능 구현

---
+++ src/main/java/dandi/dandi/auth/exception/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package dandi.dandi.auth.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
