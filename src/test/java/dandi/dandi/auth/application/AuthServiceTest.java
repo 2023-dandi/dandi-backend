6fae0f57ace4af02d12f4f7900a97ce5968d3cc9
Chris <bjuuuu98@gmail.com>
2023-02-06 15:14:07 +0900

    feat: 로그인/회원가입 기능 구현

---
+++ src/test/java/dandi/dandi/auth/application/AuthServiceTest.java
@@ -0,0 +1,81 @@
+package dandi.dandi.auth.application;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import dandi.dandi.auth.application.dto.LoginRequest;
+import dandi.dandi.auth.application.dto.LoginResponse;
+import dandi.dandi.auth.domain.OAuthClient;
+import dandi.dandi.auth.infrastructure.token.JwtTokenManager;
+import dandi.dandi.member.domain.Member;
+import dandi.dandi.member.domain.MemberRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+class AuthServiceTest {
+
+    private static final String ID_TOKEN = "idToken";
+    private static final String OAUTH_MEMBER_ID = "oAuthMemberId";
+    private static final String TOKEN = "token";
+
+    @MockBean
+    private MemberRepository memberRepository;
+
+    @MockBean
+    private OAuthClient oAuthClient;
+
+    @MockBean
+    private JwtTokenManager jwtTokenManager;
+
+    @Autowired
+    private AuthService authService;
+
+    @DisplayName("oAuthId를 받아, 새 회원이라면 회원 가입을 시키고 토큰과 새 회원 여부를 반환한다.")
+    @Test
+    void getAccessToken_NewMember() {
+        when(oAuthClient.getOAuthMemberId(anyString()))
+                .thenReturn(OAUTH_MEMBER_ID);
+        when(memberRepository.findByOAuthId(OAUTH_MEMBER_ID))
+                .thenReturn(Optional.empty());
+        when(jwtTokenManager.generateToken(anyString()))
+                .thenReturn(TOKEN);
+        when(memberRepository.save(any()))
+                .thenReturn(new Member(OAUTH_MEMBER_ID));
+
+        LoginResponse loginResponse = authService.getAccessToken(new LoginRequest(ID_TOKEN));
+
+        assertAll(
+                () -> assertThat(loginResponse.getToken()).isEqualTo(TOKEN),
+                () -> assertThat(loginResponse.isNewUser()).isTrue()
+        );
+    }
+
+    @DisplayName("oAuthId를 받아, 기존 회원이라면 회원 가입 없이 토큰과 새 회원 여부를 반환한다.")
+    @Test
+    void getAccessToken_ExistingMember() {
+        when(oAuthClient.getOAuthMemberId(anyString()))
+                .thenReturn(OAUTH_MEMBER_ID);
+        when(memberRepository.findByOAuthId(OAUTH_MEMBER_ID))
+                .thenReturn(Optional.of(new Member(OAUTH_MEMBER_ID)));
+        when(jwtTokenManager.generateToken(anyString()))
+                .thenReturn(TOKEN);
+
+        LoginResponse loginResponse = authService.getAccessToken(new LoginRequest(ID_TOKEN));
+
+        assertAll(
+                () -> assertThat(loginResponse.getToken()).isEqualTo(TOKEN),
+                () -> assertThat(loginResponse.isNewUser()).isFalse()
+        );
+    }
+}
