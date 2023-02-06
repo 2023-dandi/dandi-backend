a0a91c402b712477386d36c4ebf416e10e2bd4f1
Chris <bjuuuu98@gmail.com>
2023-02-06 23:13:01 +0900

    refactor: AuthControlle의 @controller를 @RestController로 변경

--- src/main/java/dandi/dandi/auth/presentation/AuthController.java
+++ src/main/java/dandi/dandi/auth/presentation/AuthController.java
@@ -6,11 +6,11 @@ import dandi.dandi.auth.application.dto.LoginResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AuthController {
 
     private final AuthService authService;
