8e65e24442d609866b012d394e0b2830e15921a9
Chris <bjuuuu98@gmail.com>
2023-02-09 22:56:51 +0900

    feat: 회원가입 시에 랜덤 닉네임 생성하는 기능 구현

--- src/main/java/dandi/dandi/auth/application/AuthService.java
+++ src/main/java/dandi/dandi/auth/application/AuthService.java
@@ -6,6 +6,7 @@ import dandi.dandi.auth.domain.OAuthClient;
 import dandi.dandi.auth.infrastructure.token.JwtTokenManager;
 import dandi.dandi.member.domain.Member;
 import dandi.dandi.member.domain.MemberRepository;
+import dandi.dandi.member.domain.nicknamegenerator.NicknameGenerator;
 import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -16,11 +17,14 @@ public class AuthService {
     private final OAuthClient oAuthClient;
     private final MemberRepository memberRepository;
     private final JwtTokenManager jwtTokenManager;
+    private final NicknameGenerator nicknameGenerator;
 
-    public AuthService(OAuthClient oAuthClient, MemberRepository memberRepository, JwtTokenManager jwtTokenManager) {
+    public AuthService(OAuthClient oAuthClient, MemberRepository memberRepository, JwtTokenManager jwtTokenManager,
+                       NicknameGenerator nicknameGenerator) {
         this.oAuthClient = oAuthClient;
         this.memberRepository = memberRepository;
         this.jwtTokenManager = jwtTokenManager;
+        this.nicknameGenerator = nicknameGenerator;
     }
 
     @Transactional
@@ -36,8 +40,16 @@ public class AuthService {
     }
 
     private LoginResponse getNewMemberLoginResponse(String oAuthMemberId) {
-        Member newMember = memberRepository.save(new Member(oAuthMemberId));
+        Member newMember = saveNewMember(oAuthMemberId);
         String memberId = String.valueOf(newMember.getId());
         return LoginResponse.newUser(jwtTokenManager.generateToken(memberId));
+    }
+
+    private Member saveNewMember(String oAuthMemberId) {
+        String randomNickname = nicknameGenerator.generate();
+        while (memberRepository.existsMemberByNicknameValue(randomNickname)) {
+            randomNickname = nicknameGenerator.generate();
+        }
+        return memberRepository.save(new Member(oAuthMemberId, randomNickname));
     }
 }
