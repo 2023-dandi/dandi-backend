d53d03745503c8bfb46d32dab51a53f7da390096
Chris <bjuuuu98@gmail.com>
2023-02-13 17:12:30 +0900

    feat: bad request 처리하는 ExceptionHandler 구현

--- src/main/java/dandi/dandi/advice/ExceptionResponseHandler.java
+++ src/main/java/dandi/dandi/advice/ExceptionResponseHandler.java
@@ -1,5 +1,6 @@
 package dandi.dandi.advice;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import dandi.dandi.auth.exception.UnauthorizedException;
@@ -15,6 +16,13 @@ public class ExceptionResponseHandler {
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ExceptionResponse> unauthorized(UnauthorizedException exception) {
         return ResponseEntity.status(UNAUTHORIZED)
+                .body(new ExceptionResponse(exception.getMessage()));
+    }
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ExceptionResponse> badRequest(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(exception.getMessage()));
     }
 }
