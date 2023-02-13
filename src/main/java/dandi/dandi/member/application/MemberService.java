bbd7a9ecec1529110803049f887fa9959dcf217f
Chris <bjuuuu98@gmail.com>
2023-02-13 16:48:37 +0900

    feat: 닉네임 변경 기능 application 레이어 구현

--- src/main/java/dandi/dandi/member/application/MemberService.java
+++ src/main/java/dandi/dandi/member/application/MemberService.java
@@ -2,6 +2,7 @@ package dandi.dandi.member.application;
 
 import dandi.dandi.auth.exception.UnauthorizedException;
 import dandi.dandi.member.application.dto.MemberInfoResponse;
+import dandi.dandi.member.application.dto.NicknameUpdateRequest;
 import dandi.dandi.member.domain.Member;
 import dandi.dandi.member.domain.MemberRepository;
 import org.springframework.stereotype.Service;
@@ -18,8 +19,18 @@ public class MemberService {
 
     @Transactional(readOnly = true)
     public MemberInfoResponse findMemberInfo(Long memberId) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(UnauthorizedException::notExistentMember);
+        Member member = findMember(memberId);
         return new MemberInfoResponse(member);
+    }
+
+    @Transactional
+    public void updateNickname(Long memberId, NicknameUpdateRequest nicknameUpdateRequest) {
+        Member member = findMember(memberId);
+        member.updateNickname(nicknameUpdateRequest.getNewNickname());
+    }
+
+    private Member findMember(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(UnauthorizedException::notExistentMember);
     }
 }
---
+++ src/main/java/dandi/dandi/member/application/dto/NicknameUpdateRequest.java
@@ -0,0 +1,20 @@
+package dandi.dandi.member.application.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public class NicknameUpdateRequest {
+
+    @Schema(example = "new Nickname")
+    private String newNickname;
+
+    public NicknameUpdateRequest() {
+    }
+
+    public NicknameUpdateRequest(String newNickname) {
+        this.newNickname = newNickname;
+    }
+
+    public String getNewNickname() {
+        return newNickname;
+    }
+}
