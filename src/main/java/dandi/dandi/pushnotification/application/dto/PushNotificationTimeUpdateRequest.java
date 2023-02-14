8839b33fc2ad3016308ec071c5b0051aa6c14f36
Chris <bjuuuu98@gmail.com>
2023-02-14 20:47:48 +0900

    feat: 푸시 알림 변경 시간 api pattern 지정

--- src/main/java/dandi/dandi/pushnotification/application/dto/PushNotificationTimeUpdateRequest.java
+++ src/main/java/dandi/dandi/pushnotification/application/dto/PushNotificationTimeUpdateRequest.java
@@ -1,9 +1,11 @@
 package dandi.dandi.pushnotification.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 
 public class PushNotificationTimeUpdateRequest {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm", timezone = "Asia/Seoul")
     private LocalTime newPushNotificationTime;
 
     public PushNotificationTimeUpdateRequest() {
