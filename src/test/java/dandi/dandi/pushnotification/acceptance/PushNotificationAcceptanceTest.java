67a3861ec68d0a93c5a86dfb34dac6fb85dbfe41
Chris <bjuuuu98@gmail.com>
2023-02-14 20:42:57 +0900

    feat: 푸시 알림 시간 변경 기능 presentation 레이어 구현

--- src/test/java/dandi/dandi/pushnotification/acceptance/PushNotificationAcceptanceTest.java
+++ src/test/java/dandi/dandi/pushnotification/acceptance/PushNotificationAcceptanceTest.java
@@ -1,5 +1,8 @@
 package dandi.dandi.pushnotification.acceptance;
 
+import static dandi.dandi.common.HttpMethodFixture.httpGetWithAuthorization;
+import static dandi.dandi.common.HttpMethodFixture.httpPatchWithAuthorization;
+import static dandi.dandi.common.HttpResponseExtractor.extractExceptionMessage;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.when;
@@ -9,6 +12,7 @@ import dandi.dandi.auth.application.dto.LoginRequest;
 import dandi.dandi.common.AcceptanceTest;
 import dandi.dandi.common.HttpMethodFixture;
 import dandi.dandi.pushnotification.application.dto.PushNotificationResponse;
+import dandi.dandi.pushnotification.application.dto.PushNotificationTimeUpdateRequest;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.time.LocalTime;
@@ -19,6 +23,8 @@ import org.springframework.http.HttpStatus;
 class PushNotificationAcceptanceTest extends AcceptanceTest {
 
     private static final String LOGIN_REQUEST_URI = "/login/oauth/apple";
+    private static final String PUSH_NOTIFICATION_REQUEST_URI = "/push-notification";
+    private static final String PUSH_NOTIFICATION_TIME_REQUEST_URI = "/push-notification/time";
 
     @DisplayName("회원의 푸시 알림 정보(시간대, 허용 여부)의 요청에 성공하면 200과 푸시 알림 정보를 반환한다.")
     @Test
@@ -30,7 +36,7 @@ class PushNotificationAcceptanceTest extends AcceptanceTest {
                 .header(AUTHORIZATION);
 
         ExtractableResponse<Response> response =
-                HttpMethodFixture.httpGetWithAuthorization("/push-notification", token);
+                httpGetWithAuthorization(PUSH_NOTIFICATION_REQUEST_URI, token);
 
         PushNotificationResponse pushNotificationResponse = response.jsonPath()
                 .getObject(".", PushNotificationResponse.class);
@@ -38,6 +44,54 @@ class PushNotificationAcceptanceTest extends AcceptanceTest {
                 () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
                 () -> assertThat(pushNotificationResponse.getPushNotificationTime()).isEqualTo(LocalTime.MIN),
                 () -> assertThat(pushNotificationResponse.isAllowance()).isFalse()
+        );
+    }
+
+    @DisplayName("회원의 푸시 알림 시간 변경 요청에 성공하면 204를 반환한다.")
+    @Test
+    void updatePushNotificationTime_NoContent() {
+        String oAuthIdToken = "idToken";
+        when(oAuthClient.getOAuthMemberId(oAuthIdToken))
+                .thenReturn("memberIdentifier");
+        String token = HttpMethodFixture.httpPost(new LoginRequest(oAuthIdToken), LOGIN_REQUEST_URI)
+                .header(AUTHORIZATION);
+        LocalTime initialPushNotificationTime = httpGetWithAuthorization(PUSH_NOTIFICATION_REQUEST_URI, token)
+                .jsonPath()
+                .getObject(".", PushNotificationResponse.class)
+                .getPushNotificationTime();
+        PushNotificationTimeUpdateRequest pushNotificationTimeUpdateRequest =
+                new PushNotificationTimeUpdateRequest(LocalTime.of(20, 10));
+
+        ExtractableResponse<Response> response = httpPatchWithAuthorization(
+                PUSH_NOTIFICATION_TIME_REQUEST_URI, pushNotificationTimeUpdateRequest, token);
+
+        LocalTime pushNotificationTimeAfterUpdate = httpGetWithAuthorization(PUSH_NOTIFICATION_REQUEST_URI, token)
+                .jsonPath()
+                .getObject(".", PushNotificationResponse.class)
+                .getPushNotificationTime();
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value()),
+                () -> assertThat(initialPushNotificationTime).isNotEqualTo(pushNotificationTimeAfterUpdate)
+        );
+    }
+
+    @DisplayName("10분 단위가 아닌 푸시 알림 시간 변경 요청에 대해 400을 반환한다.")
+    @Test
+    void updatePushNotificationTime_BadRequest() {
+        String oAuthIdToken = "idToken";
+        when(oAuthClient.getOAuthMemberId(oAuthIdToken))
+                .thenReturn("memberIdentifier");
+        String token = HttpMethodFixture.httpPost(new LoginRequest(oAuthIdToken), LOGIN_REQUEST_URI)
+                .header(AUTHORIZATION);
+        PushNotificationTimeUpdateRequest invalidPushNotificationTimeUpdateRequest =
+                new PushNotificationTimeUpdateRequest(LocalTime.of(20, 11));
+
+        ExtractableResponse<Response> response = httpPatchWithAuthorization(
+                PUSH_NOTIFICATION_TIME_REQUEST_URI, invalidPushNotificationTimeUpdateRequest, token);
+
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+                () -> assertThat(extractExceptionMessage(response)).isNotNull()
         );
     }
 }
