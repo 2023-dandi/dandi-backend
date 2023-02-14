9b13e77be593a3910517930fdb8c3979af892916
Chris <bjuuuu98@gmail.com>
2023-02-14 16:05:16 +0900

    feat: 푸시 알림 정보 반환 기능 domain 레이어 구현

--- src/main/java/dandi/dandi/pushnotification/domain/PushNotificationRepository.java
+++ src/main/java/dandi/dandi/pushnotification/domain/PushNotificationRepository.java
@@ -1,6 +1,9 @@
 package dandi.dandi.pushnotification.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PushNotificationRepository extends JpaRepository<PushNotification, Long> {
+
+    Optional<PushNotification> findPushNotificationByMemberId(Long memberId);
 }
