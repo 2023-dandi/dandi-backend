53f3cd44eccb60a3eec280c36a0b6624a5659f44
Chris <bjuuuu98@gmail.com>
2023-02-14 20:48:46 +0900

    docs: 푸시 알림 시간 변경 api 문서화

--- src/main/java/dandi/dandi/pushnotification/application/dto/PushNotificationTimeUpdateRequest.java
+++ src/main/java/dandi/dandi/pushnotification/application/dto/PushNotificationTimeUpdateRequest.java
@@ -1,11 +1,13 @@
 package dandi.dandi.pushnotification.application.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalTime;
 
 public class PushNotificationTimeUpdateRequest {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm", timezone = "Asia/Seoul")
+    @Schema(example = "20:30")
     private LocalTime newPushNotificationTime;
 
     public PushNotificationTimeUpdateRequest() {
--- src/main/java/dandi/dandi/pushnotification/presentation/PushNotificationControllerDocs.java
+++ src/main/java/dandi/dandi/pushnotification/presentation/PushNotificationControllerDocs.java
@@ -3,10 +3,12 @@ package dandi.dandi.pushnotification.presentation;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 import dandi.dandi.pushnotification.application.dto.PushNotificationResponse;
+import dandi.dandi.pushnotification.application.dto.PushNotificationTimeUpdateRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
@@ -16,4 +18,12 @@ public interface PushNotificationControllerDocs {
     @Operation(summary = "푸시 알림 정보 반환", parameters = @Parameter(name = AUTHORIZATION, in = ParameterIn.HEADER, required = true))
     @ApiResponse(responseCode = "200", description = "푸시 알림 정보 정상 반환")
     ResponseEntity<PushNotificationResponse> getPushNotification(@Parameter(hidden = true) Long memberId);
+
+    @Operation(summary = "푸시 알림 시간 변경", parameters = @Parameter(name = AUTHORIZATION, in = ParameterIn.HEADER, required = true))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "푸시 알림 시간 정상 변경"),
+            @ApiResponse(responseCode = "400", description = "10분 단위가 아닌 푸시 알림 시간")
+    })
+    ResponseEntity<Void> updatePushNotificationTime(@Parameter(hidden = true) Long memberId,
+                                                    PushNotificationTimeUpdateRequest pushNotificationTimeUpdateRequest);
 }
