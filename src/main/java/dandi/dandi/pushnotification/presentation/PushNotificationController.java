92e0ee6316390e15b6105aa3f5ceb9fddcad23f0
Chris <bjuuuu98@gmail.com>
2023-02-14 17:02:23 +0900

    feat: 푸시 알림 정보 반환 기능 presentation 레이어 구현

---
+++ src/main/java/dandi/dandi/pushnotification/presentation/PushNotificationController.java
@@ -0,0 +1,23 @@
+package dandi.dandi.pushnotification.presentation;
+
+import dandi.dandi.auth.support.Login;
+import dandi.dandi.pushnotification.application.PushNotificationService;
+import dandi.dandi.pushnotification.application.dto.PushNotificationResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PushNotificationController {
+
+    private final PushNotificationService pushNotificationService;
+
+    public PushNotificationController(PushNotificationService pushNotificationService) {
+        this.pushNotificationService = pushNotificationService;
+    }
+
+    @GetMapping("/push-notification")
+    public ResponseEntity<PushNotificationResponse> getPushNotification(@Login Long memberId) {
+        return ResponseEntity.ok(pushNotificationService.findPushNotification(memberId));
+    }
+}
