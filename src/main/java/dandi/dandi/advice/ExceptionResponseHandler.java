e4807a0d661f744ab3492ceaf1f1921e9a4f1285
Chris <bjuuuu98@gmail.com>
2023-03-03 16:06:05 +0900

    feat: 500 에러를 로깅하는 기능 구현

--- src/main/java/dandi/dandi/advice/ExceptionResponseHandler.java
+++ src/main/java/dandi/dandi/advice/ExceptionResponseHandler.java
@@ -3,12 +3,17 @@ package dandi.dandi.advice;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import dandi.dandi.auth.exception.UnauthorizedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionResponseHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionResponseHandler.class);
+    private static final String INTERNAL_SERVER_ERROR_LOGGING_FORMAT = "[500]" + System.lineSeparator() + "{}";
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ExceptionResponse> unauthorized(UnauthorizedException exception) {
@@ -23,7 +28,8 @@ public class ExceptionResponseHandler {
     }
 
     @ExceptionHandler({InternalServerException.class, RuntimeException.class})
-    public ResponseEntity<ExceptionResponse> internalServerError() {
+    public ResponseEntity<ExceptionResponse> internalServerError(RuntimeException e) {
+        logger.warn(INTERNAL_SERVER_ERROR_LOGGING_FORMAT, e.getMessage());
         return ResponseEntity.internalServerError()
                 .body(ExceptionResponse.internalServerError());
     }
