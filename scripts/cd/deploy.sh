82fc18c26e66587e913debea5ed91378c40bcdab
Chris <bjuuuu98@gmail.com>
2023-03-10 22:25:04 +0900

    cd: jar 파일 접근 cd에서 cp 방식으로 변경

--- scripts/cd/deploy.sh
+++ scripts/cd/deploy.sh
@@ -1,12 +1,14 @@
-PROJECT_ROOT="/home/ubuntu/dandi-project"
-DEPLOY_LOG="$PROJECT_ROOT/deploy-log"
-
+ROOT_PATH="/home/ubuntu"
+DEPLOY_LOG="$ROOT_PATH/deploy.log"
+ORIGINAL_JAR_FILE="$ROOT_PATH/dandi-project/build/libs/*.jar"
+COPIED_JAR_FILE="$ROOT_PATH/dandi.jar"
 CURRENT_TIME=$(date +%c)
-cd $PROJECT_ROOT/build/libs
+
+cp $ORIGINAL_JAR_FILE $COPIED_JAR_FILE
 
 nohup java -jar -Dspring.profiles.active=dev -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -Dlog4j2.enable.threadlocals=true -Dlog4j2.enable.direct.encoders=true *.jar &
 echo "$CURRENT_TIME > jar 파일 실행" >>$DEPLOY_LOG
 
-EXECUTED_PROCESS_PID=$(pgrep -f $JAR_FILE)
+EXECUTED_PROCESS_PID=$(pgrep -f *.jar)
 echo "$CURRENT_TIME > executed process pid = $EXECUTED_PROCESS_PID" >>$DEPLOY_LOG
 curl -s -d "payload={\"text\":\"Application Execution : $EXECUTED_PROCESS_PID\"}" "${SLACK_WEBHOOK_URI}"
