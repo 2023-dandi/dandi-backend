78a17f7a665af946dab1b9521620a5368fca2e29
Chris <bjuuuu98@gmail.com>
2023-03-10 18:38:53 +0900

    cd: 불필요한 cd 스크립트 변수 삭제

--- scripts/cd/deploy.sh
+++ scripts/cd/deploy.sh
@@ -1,14 +1,11 @@
 PROJECT_ROOT="/home/ubuntu/dandi-project"
-JAR_FILE="$PROJECT_ROOT/dandi.jar"
-
-APP_LOG="$PROJECT_ROOT/application-log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy-log"
 
 CURRENT_TIME=$(date +%c)
 cd $PROJECT_ROOT/build/libs
 
-nohup java -jar "-Dspring.profiles.active=dev \
--DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector \
+nohup java -jar "-Dspring.profiles.active=dev\
+-DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector\
 -Dlog4j2.enable.threadlocals=true -Dlog4j2.enable.direct.encoders=true" *.jar &
 echo "$CURRENT_TIME > jar 파일 실행" >>$DEPLOY_LOG
 
--- scripts/cd/terminate.sh
+++ scripts/cd/terminate.sh
@@ -1,11 +1,8 @@
 PROJECT_ROOT="/home/ubuntu/dandi-project"
-JAR_FILE="$PROJECT_ROOT/dandi.jar"
-
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
-
 TIME_NOW=$(date +%c)
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f *.jar)
 
 if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > no process" >> $DEPLOY_LOG
