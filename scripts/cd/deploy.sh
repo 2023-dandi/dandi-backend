1dccbc3a7b66227badbbee76d7ee3d62f8d65434
Chris <bjuuuu98@gmail.com>
2023-03-10 22:30:56 +0900

    cd: nohup 파일 생성하지 않도록 변경

--- scripts/cd/deploy.sh
+++ scripts/cd/deploy.sh
@@ -6,7 +6,7 @@ CURRENT_TIME=$(date +%c)
 
 cp $ORIGINAL_JAR_FILE $COPIED_JAR_FILE
 
-nohup java -jar -Dspring.profiles.active=dev -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -Dlog4j2.enable.threadlocals=true -Dlog4j2.enable.direct.encoders=true *.jar &
+nohup java -jar -Dspring.profiles.active=dev -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -Dlog4j2.enable.threadlocals=true -Dlog4j2.enable.direct.encoders=true *.jar >/dev/null &
 echo "$CURRENT_TIME > jar 파일 실행" >>$DEPLOY_LOG
 
 EXECUTED_PROCESS_PID=$(pgrep -f *.jar)
