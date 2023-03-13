6003aac6782b3bdeb9cdafd25527940e77027063
Chris <bjuuuu98@gmail.com>
2023-03-13 14:20:36 +0900

    cd: 배포 스크립트 작성

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+ROOT_PATH="/home/ubuntu"
+DEPLOY_LOG="$ROOT_PATH/deploy.log"
+ORIGINAL_JAR_FILE="$ROOT_PATH/dandi-project/build/libs/*.jar"
+
+CURRENT_TIME=$(date +%c)
+
+cp $ORIGINAL_JAR_FILE $ROOT_PATH
+
+cd $ROOT_PATH
+nohup java -jar -Dspring.profiles.active=dev -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -Dlog4j2.enable.threadlocals=true -Dlog4j2.enable.direct.encoders=true *.jar /dev/null 2> /dev/null < /dev/null &
+echo "$CURRENT_TIME > jar 파일 실행" >>$DEPLOY_LOG
+
+EXECUTED_PROCESS_PID=$(lsof -t -i tcp:8080)
+echo "$CURRENT_TIME > executed process pid = $EXECUTED_PROCESS_PID" >>$DEPLOY_LOG
+curl -s -d "payload={\"text\":\"Application Execution : $EXECUTED_PROCESS_PID\"}" "${SLACK_WEBHOOK_URI}"
