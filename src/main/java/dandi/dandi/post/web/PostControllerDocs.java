61389e583f7f481486050235ade70f8fc6c1ecae
Chris <bjuuuu98@gmail.com>
2023-03-17 19:41:50 +0900

    docs: 날씨에 따른 착장 느낌 Index 범위 변경 문서화

--- src/main/java/dandi/dandi/post/web/PostControllerDocs.java
+++ src/main/java/dandi/dandi/post/web/PostControllerDocs.java
@@ -47,7 +47,8 @@ public interface PostControllerDocs {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "닉네임 정상 변경", headers = {
                     @Header(name = HttpHeaders.LOCATION)}),
-            @ApiResponse(responseCode = "400", description = "입력 값 중에 null 혹은 빈문자열이 존재할 경우",
+            @ApiResponse(responseCode = "400", description = "입력 값 중에 null 혹은 빈문자열이 존재할 경우 \r\n"
+                    + "착장 느낌 Index가 0 ~ 4 범위가 아닌 경우",
                     content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))
     })
     ResponseEntity<Void> registerPost(@Parameter(hidden = true) Long memberId, PostRegisterRequest postRegisterRequest);
