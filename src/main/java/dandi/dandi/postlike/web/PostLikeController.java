a631540053e05ab9f1885b7e9a4f0ae7e2eb7eba
Chris <bjuuuu98@gmail.com>
2023-03-20 12:18:06 +0900

    docs: 게시글 좋아요 기능 문서화

--- src/main/java/dandi/dandi/postlike/web/PostLikeController.java
+++ src/main/java/dandi/dandi/postlike/web/PostLikeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PostLikeController {
+public class PostLikeController implements PostLikeControllerDocs {
 
     private final PostLikeUseCase postLikeUseCase;
 
