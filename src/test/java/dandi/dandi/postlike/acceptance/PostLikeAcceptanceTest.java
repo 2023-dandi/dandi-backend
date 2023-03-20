97b1593fb72143366240a1f98883fc77c3f24ee8
Chris <bjuuuu98@gmail.com>
2023-03-20 21:08:35 +0900

    test: 좋아요 등록/취소 인수테스트에 게시글 상세 조회해서 등록/취소 되었는지 확인 로직 추가

--- src/test/java/dandi/dandi/postlike/acceptance/PostLikeAcceptanceTest.java
+++ src/test/java/dandi/dandi/postlike/acceptance/PostLikeAcceptanceTest.java
@@ -1,9 +1,12 @@
 package dandi.dandi.postlike.acceptance;
 
+import static dandi.dandi.common.HttpMethodFixture.httpGetWithAuthorization;
+import static dandi.dandi.common.HttpMethodFixture.httpPatchWithAuthorization;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import dandi.dandi.common.AcceptanceTest;
-import dandi.dandi.common.HttpMethodFixture;
+import dandi.dandi.post.application.port.in.PostDetailResponse;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -17,23 +20,47 @@ class PostLikeAcceptanceTest extends AcceptanceTest {
     void reverseLikes_Register_NoContent() {
         String token = getToken();
         Long postId = registerPost(token);
+        boolean likedBeforePostLikeRequest = httpGetWithAuthorization("/posts/" + postId, token)
+                .jsonPath()
+                .getObject(".", PostDetailResponse.class)
+                .isLiked();
 
-        ExtractableResponse<Response> response = HttpMethodFixture.httpPatchWithAuthorization(
+        ExtractableResponse<Response> response = httpPatchWithAuthorization(
                 "/posts/" + postId + "/likes", token);
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        boolean likedAfterPostLikeRequest = httpGetWithAuthorization("/posts/" + postId, token)
+                .jsonPath()
+                .getObject(".", PostDetailResponse.class)
+                .isLiked();
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value()),
+                () -> assertThat(likedBeforePostLikeRequest).isFalse(),
+                () -> assertThat(likedAfterPostLikeRequest).isTrue()
+        );
     }
 
     @DisplayName("자신이 좋아요를 누른 게시글에 좋아요를 취소할 수 있다.")
     @Test
-    void reverseLikes_Deletion_NoContent() {
+    void reverseLikes_Cancel_NoContent() {
         String token = getToken();
         Long postId = registerPost(token);
-        HttpMethodFixture.httpPatchWithAuthorization("/posts/" + postId + "/likes", token);
+        httpPatchWithAuthorization("/posts/" + postId + "/likes", token);
+        boolean likedBeforePostLikeRequest = httpGetWithAuthorization("/posts/" + postId, token)
+                .jsonPath()
+                .getObject(".", PostDetailResponse.class)
+                .isLiked();
 
-        ExtractableResponse<Response> response = HttpMethodFixture.httpPatchWithAuthorization(
+        ExtractableResponse<Response> response = httpPatchWithAuthorization(
                 "/posts/" + postId + "/likes", token);
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        boolean likedAfterPostLikeRequest = httpGetWithAuthorization("/posts/" + postId, token)
+                .jsonPath()
+                .getObject(".", PostDetailResponse.class)
+                .isLiked();
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value()),
+                () -> assertThat(likedBeforePostLikeRequest).isTrue(),
+                () -> assertThat(likedAfterPostLikeRequest).isFalse()
+        );
     }
 }
