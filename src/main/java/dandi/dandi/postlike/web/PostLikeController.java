e2fe768434cf50c52f80c88817d380aa2f74b46a
Chris <bjuuuu98@gmail.com>
2023-03-20 12:15:12 +0900

    feat: 게시글 좋아요 기능 web 구현

---
+++ src/main/java/dandi/dandi/postlike/web/PostLikeController.java
@@ -0,0 +1,24 @@
+package dandi.dandi.postlike.web;
+
+import dandi.dandi.auth.web.support.Login;
+import dandi.dandi.postlike.application.port.in.PostLikeUseCase;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostLikeController {
+
+    private final PostLikeUseCase postLikeUseCase;
+
+    public PostLikeController(PostLikeUseCase postLikeUseCase) {
+        this.postLikeUseCase = postLikeUseCase;
+    }
+
+    @PatchMapping("/posts/{postId}/likes")
+    public ResponseEntity<Void> reverseLikes(@Login Long memberId, @PathVariable Long postId) {
+        postLikeUseCase.reverseLike(memberId, postId);
+        return ResponseEntity.noContent().build();
+    }
+}
