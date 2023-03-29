0779a96f26ff4df092136f1ea28fff0f12ea0ba3
Chris <bjuuuu98@gmail.com>
2023-03-30 00:02:06 +0900

    test: PostLikePersistenceAdapter 테스트 작성

---
+++ src/test/java/dandi/dandi/postlike/adapter/PostLikePersistenceAdapterTest.java
@@ -0,0 +1,69 @@
+package dandi.dandi.postlike.adapter;
+
+import static dandi.dandi.member.MemberTestFixture.MEMBER_ID;
+import static dandi.dandi.post.PostFixture.POST_ID;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import dandi.dandi.common.PersistenceAdapterTest;
+import dandi.dandi.postlike.domain.PostLike;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class PostLikePersistenceAdapterTest extends PersistenceAdapterTest {
+
+    @Autowired
+    private PostLikePersistenceAdapter postLikePersistenceAdapter;
+
+    @DisplayName("게시글 좋아요를 저장할 수 있다.")
+    @Test
+    void save() {
+        PostLike postLike = PostLike.initial(MEMBER_ID, POST_ID);
+
+        assertThatCode(() -> postLikePersistenceAdapter.save(postLike))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("회원과 게시글에 해당하는 게시글 좋아요를 찾을 수 있다.")
+    @ParameterizedTest
+    @CsvSource({"1, 1, true", "1, 2, false", "2, 1, false"})
+    void findByMemberIdAndPostId(Long memberId, Long postId, boolean expectedPresentation) {
+        PostLike postLike = PostLike.initial(MEMBER_ID, POST_ID);
+        postLikePersistenceAdapter.save(postLike);
+
+        Optional<PostLike> actual = postLikePersistenceAdapter.findByMemberIdAndPostId(memberId, postId);
+
+        assertThat(actual.isPresent()).isEqualTo(expectedPresentation);
+    }
+
+    @DisplayName("id로 게시글 좋아요를 삭제할 수 있다.")
+    @Test
+    void deleteById() {
+        PostLike postLike = PostLike.initial(MEMBER_ID, POST_ID);
+        postLikePersistenceAdapter.save(postLike);
+        Long postLikeId = postLikePersistenceAdapter.findByMemberIdAndPostId(MEMBER_ID, POST_ID)
+                .get()
+                .getId();
+
+        postLikePersistenceAdapter.deleteById(postLikeId);
+
+        Optional<PostLike> foundAfterDeletion = postLikePersistenceAdapter.findByMemberIdAndPostId(MEMBER_ID, POST_ID);
+        assertThat(foundAfterDeletion).isEmpty();
+    }
+
+    @DisplayName("회원과 게시글에 해당하는 게시글 좋아요가 존재하는지 반환할 수 있다.")
+    @ParameterizedTest
+    @CsvSource({"1, 1, true", "1, 2, false", "2, 1, false"})
+    void existsByPostIdAndMemberId(Long memberId, Long postId, boolean expected) {
+        PostLike postLike = PostLike.initial(MEMBER_ID, POST_ID);
+        postLikePersistenceAdapter.save(postLike);
+
+        boolean actual = postLikePersistenceAdapter.existsByPostIdAndMemberId(memberId, postId);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
