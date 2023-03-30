90400049293c3b0b3c83b61dfe45284ebddd1968
Chris <bjuuuu98@gmail.com>
2023-03-30 17:12:36 +0900

    refactor: 옷 계절 엔티티 테이블 이름 변경

--- src/main/java/dandi/dandi/clothes/adapter/persistence/ClothesSeasonJpaEntity.java
+++ src/main/java/dandi/dandi/clothes/adapter/persistence/ClothesSeasonJpaEntity.java
@@ -7,8 +7,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "clothes_season")
 public class ClothesSeasonJpaEntity {
 
     @Id
