94019ffd884a03f6b1396c57c3c2909a690a6b24
Chris <bjuuuu98@gmail.com>
2023-03-30 19:25:10 +0900

    test: Clothes 정적팩토리 변경에 따른 테스트코드 수정

--- src/test/java/dandi/dandi/clothes/domain/ClothesTest.java
+++ src/test/java/dandi/dandi/clothes/domain/ClothesTest.java
@@ -1,11 +1,10 @@
 package dandi.dandi.clothes.domain;
 
+import static dandi.dandi.clothes.ClothesFixture.CLOTHES_CATEGORY;
 import static dandi.dandi.clothes.ClothesFixture.CLOTHES_IMAGE_URL;
-import static dandi.dandi.clothes.domain.Category.TOP;
-import static dandi.dandi.clothes.domain.Season.SUMMER;
+import static dandi.dandi.clothes.ClothesFixture.CLOTHES_SEASONS;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -16,7 +15,7 @@ class ClothesTest {
     @ParameterizedTest
     @CsvSource({"1, true", "2, false"})
     void isOwnedBy(Long memberId, boolean expected) {
-        Clothes clothes = Clothes.initial(1L, TOP, List.of(SUMMER), CLOTHES_IMAGE_URL);
+        Clothes clothes = Clothes.initial(1L, CLOTHES_CATEGORY, CLOTHES_SEASONS, CLOTHES_IMAGE_URL);
 
         boolean actual = clothes.isOwnedBy(memberId);
 
