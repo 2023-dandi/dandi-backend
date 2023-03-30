b32e2f8be266a9f63aa7f3aedea3137bbc7424ea
Chris <bjuuuu98@gmail.com>
2023-03-30 18:02:16 +0900

    feat: ClothesUseCase에 옷 삭제 기능 정의

--- src/main/java/dandi/dandi/clothes/application/port/in/ClothesUseCase.java
+++ src/main/java/dandi/dandi/clothes/application/port/in/ClothesUseCase.java
@@ -3,4 +3,6 @@ package dandi.dandi.clothes.application.port.in;
 public interface ClothesUseCase {
 
     void registerClothes(Long memberId, ClothesRegisterCommand clothesRegisterCommand);
+
+    void deleteClothes(Long memberId, Long clothesId);
 }
