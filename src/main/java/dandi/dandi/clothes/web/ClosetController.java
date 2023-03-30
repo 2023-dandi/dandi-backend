e284627fec83e67d1057df2155027d87c125263d
Chris <bjuuuu98@gmail.com>
2023-03-30 12:29:52 +0900

    feat: 옷 사진 등록 기능 web 구현

---
+++ src/main/java/dandi/dandi/clothes/web/ClosetController.java
@@ -0,0 +1,30 @@
+package dandi.dandi.clothes.web;
+
+import dandi.dandi.auth.web.support.Login;
+import dandi.dandi.clothes.application.port.in.ClothesImageRegisterResponse;
+import dandi.dandi.clothes.application.port.in.ClothesImageUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class ClosetController {
+
+    private final ClothesImageUseCase clothesImageUseCase;
+
+    public ClosetController(ClothesImageUseCase clothesImageUseCase) {
+        this.clothesImageUseCase = clothesImageUseCase;
+    }
+
+    @PostMapping("/clothes/image")
+    public ResponseEntity<ClothesImageRegisterResponse> registerClothesImage(@Login Long memberId,
+                                                                             @RequestPart(value = "clothesImage")
+                                                                             MultipartFile clothesImage) {
+        ClothesImageRegisterResponse clothesImageRegisterResponse =
+                clothesImageUseCase.uploadClothesImage(memberId, clothesImage);
+        return ResponseEntity.status(HttpStatus.CREATED).body(clothesImageRegisterResponse);
+    }
+}
