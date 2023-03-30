c6ca16a404345c7a22329d41d356f107ff445ad0
Chris <bjuuuu98@gmail.com>
2023-03-30 16:07:00 +0900

    feat: 옷 추가 기능 web 구현

--- src/main/java/dandi/dandi/clothes/web/ClosetController.java
+++ src/main/java/dandi/dandi/clothes/web/ClosetController.java
@@ -3,9 +3,12 @@ package dandi.dandi.clothes.web;
 import dandi.dandi.auth.web.support.Login;
 import dandi.dandi.clothes.application.port.in.ClothesImageRegisterResponse;
 import dandi.dandi.clothes.application.port.in.ClothesImageUseCase;
+import dandi.dandi.clothes.application.port.in.ClothesRegisterCommand;
+import dandi.dandi.clothes.application.port.in.ClothesUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,9 +17,11 @@ import org.springframework.web.multipart.MultipartFile;
 public class ClosetController implements ClosetControllerDocs {
 
     private final ClothesImageUseCase clothesImageUseCase;
+    private final ClothesUseCase clothesUseCase;
 
-    public ClosetController(ClothesImageUseCase clothesImageUseCase) {
+    public ClosetController(ClothesImageUseCase clothesImageUseCase, ClothesUseCase clothesUseCase) {
         this.clothesImageUseCase = clothesImageUseCase;
+        this.clothesUseCase = clothesUseCase;
     }
 
     @PostMapping("/clothes/image")
@@ -26,5 +31,12 @@ public class ClosetController implements ClosetControllerDocs {
         ClothesImageRegisterResponse clothesImageRegisterResponse =
                 clothesImageUseCase.uploadClothesImage(memberId, clothesImage);
         return ResponseEntity.status(HttpStatus.CREATED).body(clothesImageRegisterResponse);
+    }
+
+    @PostMapping("/clothes")
+    public ResponseEntity<Void> registerClothes(@Login Long memberId,
+                                                @RequestBody ClothesRegisterCommand clothesRegisterCommand) {
+        clothesUseCase.registerClothes(memberId, clothesRegisterCommand);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
