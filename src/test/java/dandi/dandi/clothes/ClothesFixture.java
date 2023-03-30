e5b557627a933ee53d4935378259f5dc59bfeaa1
Chris <bjuuuu98@gmail.com>
2023-03-30 19:21:49 +0900

    test: Clothes 픽스처에 cloudfront 주소 포함된 옷 이미지 URL 선언

--- src/test/java/dandi/dandi/clothes/ClothesFixture.java
+++ src/test/java/dandi/dandi/clothes/ClothesFixture.java
@@ -3,6 +3,7 @@ package dandi.dandi.clothes;
 import static dandi.dandi.clothes.domain.Category.TOP;
 import static dandi.dandi.clothes.domain.Season.SUMMER;
 import static dandi.dandi.member.MemberTestFixture.MEMBER_ID;
+import static dandi.dandi.utils.image.TestImageUtils.IMAGE_ACCESS_URL;
 
 import dandi.dandi.clothes.domain.Clothes;
 import java.util.List;
@@ -13,6 +14,8 @@ public class ClothesFixture {
     public static final String CLOTHES_CATEGORY = "TOP";
     public static final List<String> CLOTHES_SEASONS = List.of("SPRING", "SUMMER");
     public static final String CLOTHES_IMAGE_URL = "clothesImageUrl";
+    private static final String CLOTHES_IMAGE_DIR = "clothes/";
+    public static final String CLOTHES_IMAGE_FULL_URL = IMAGE_ACCESS_URL + CLOTHES_IMAGE_DIR + CLOTHES_IMAGE_DIR;
 
     public static final Clothes CLOTHES = new Clothes(CLOTHES_ID, MEMBER_ID, TOP, List.of(SUMMER), CLOTHES_IMAGE_URL);
 }
