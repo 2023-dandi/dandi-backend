838d87a02624e7e17482c9f5286ad503674bafdb
Chris <bjuuuu98@gmail.com>
2023-03-30 15:27:21 +0900

    feat: 옷 추가 기능 persistence 구현

---
+++ src/test/java/dandi/dandi/clothes/adapter/persistence/ClothesPersistenceAdapterTest.java
@@ -0,0 +1,27 @@
+package dandi.dandi.clothes.adapter.persistence;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+import dandi.dandi.clothes.domain.Category;
+import dandi.dandi.clothes.domain.Clothes;
+import dandi.dandi.clothes.domain.Season;
+import dandi.dandi.common.PersistenceAdapterTest;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class ClothesPersistenceAdapterTest extends PersistenceAdapterTest {
+
+    @Autowired
+    private ClothesPersistenceAdapter clothesPersistenceAdapter;
+
+    @DisplayName("옷을 저장할 수 있다.")
+    @Test
+    void save() {
+        Clothes clothes = Clothes.initial(Category.TOP, List.of(Season.SPRING, Season.SUMMER), "clothesImageUrl");
+
+        assertThatCode(() -> clothesPersistenceAdapter.save(clothes, 1L))
+                .doesNotThrowAnyException();
+    }
+}
