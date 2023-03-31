cd150513ca1e2cb542f82b73772d3a6a7068c0db
Chris <bjuuuu98@gmail.com>
2023-03-31 23:45:38 +0900

    refactor: Season enum db 저장방식 ORDINAL에서 STRING으로 변경

--- src/main/java/dandi/dandi/clothes/adapter/persistence/ClothesSeasonJpaEntity.java
+++ src/main/java/dandi/dandi/clothes/adapter/persistence/ClothesSeasonJpaEntity.java
@@ -3,6 +3,8 @@ package dandi.dandi.clothes.adapter.persistence;
 import dandi.dandi.clothes.domain.Season;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -23,6 +25,7 @@ public class ClothesSeasonJpaEntity {
     @JoinColumn(name = "clothes_id")
     private ClothesJpaEntity clothesJpaEntity;
 
+    @Enumerated(value = EnumType.STRING)
     private Season season;
 
     public ClothesSeasonJpaEntity() {
