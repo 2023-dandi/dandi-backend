6856be320fc517dcb600c562d9b2bb98fff28092
Chris <bjuuuu98@gmail.com>
2023-03-31 15:27:25 +0900

    feat: 카테고리, 계절에 따른 옷 조회 기능 persistence 구현

--- src/main/java/dandi/dandi/clothes/adapter/persistence/ClothesRepository.java
+++ src/main/java/dandi/dandi/clothes/adapter/persistence/ClothesRepository.java
@@ -1,6 +1,11 @@
 package dandi.dandi.clothes.adapter.persistence;
 
+import dandi.dandi.clothes.domain.Category;
+import dandi.dandi.clothes.domain.Season;
 import java.util.Optional;
+import java.util.Set;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,4 +13,10 @@ public interface ClothesRepository extends JpaRepository<ClothesJpaEntity, Long>
 
     @Query("SELECT c FROM ClothesJpaEntity c JOIN FETCH c.seasons WHERE c.id = :id")
     Optional<ClothesJpaEntity> findById(Long id);
+
+    @Query("SELECT DISTINCT c FROM ClothesJpaEntity c "
+            + "INNER JOIN ClothesSeasonJpaEntity cs ON cs.clothesJpaEntity.id = c.id "
+            + "WHERE c.memberId = :memberId AND c.category = :category AND cs.season IN :seasons")
+    Slice<ClothesJpaEntity> findByMemberIdAndCategoryAndSeasons(Long memberId, Category category,
+                                                                Set<Season> seasons, Pageable pageable);
 }
