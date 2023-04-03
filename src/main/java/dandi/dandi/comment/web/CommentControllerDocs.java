c624a60925f2b250f68a7a46a9f5b01144717e46
Chris <bjuuuu98@gmail.com>
2023-04-03 15:59:53 +0900

    docs: 댓글 작성 기능 문서화

---
+++ src/main/java/dandi/dandi/comment/web/CommentControllerDocs.java
@@ -0,0 +1,25 @@
+package dandi.dandi.comment.web;
+
+import dandi.dandi.advice.ExceptionResponse;
+import dandi.dandi.comment.application.port.in.CommentRegisterCommand;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "댓글")
+public interface CommentControllerDocs {
+
+    @Operation(summary = "댓글 작성")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "댓글 작성 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 게시글에 댓글 작성",
+                    content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))
+    })
+    ResponseEntity<Void> registerComment(@Parameter(hidden = true) Long memberId,
+                                         CommentRegisterCommand commentRegisterCommand);
+}
