0e4fa5703eaed2e90bf6cb29a333062f2c509809
Chris <bjuuuu98@gmail.com>
2023-04-03 15:26:18 +0900

    test: 댓글 등록 기능 인수테스트 작성

---
+++ src/test/java/dandi/dandi/comment/acceptance/CommentAcceptanceTest.java
@@ -0,0 +1,39 @@
+package dandi.dandi.comment.acceptance;
+
+import static dandi.dandi.common.HttpMethodFixture.httpPostWithAuthorization;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import dandi.dandi.comment.application.port.in.CommentRegisterCommand;
+import dandi.dandi.common.AcceptanceTest;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+class CommentAcceptanceTest extends AcceptanceTest {
+
+    @DisplayName("댓글 작성 요청에 성공하면 204를 응답한다.")
+    @Test
+    void registerComment_NoContent() {
+        String token = getToken();
+        Long postId = registerPost(token);
+        CommentRegisterCommand commentRegisterCommand = new CommentRegisterCommand(postId, "댓글 내용");
+
+        ExtractableResponse<Response> response = httpPostWithAuthorization("comments", commentRegisterCommand, token);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    @DisplayName("존재하지 않는 글에 대한 댓글 작성 요청에 대해 404를 응답한다.")
+    @Test
+    void registerComment_NotFound() {
+        String token = getToken();
+        Long notFoundPostId = 1L;
+        CommentRegisterCommand commentRegisterCommand = new CommentRegisterCommand(notFoundPostId, "댓글 내용");
+
+        ExtractableResponse<Response> response = httpPostWithAuthorization("comments", commentRegisterCommand, token);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+}
