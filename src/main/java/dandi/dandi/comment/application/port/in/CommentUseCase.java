9425c88e37d8dfe393827d7d5dfa13bef91fa287
Chris <bjuuuu98@gmail.com>
2023-04-04 21:45:57 +0900

    feat: 댓글 삭제 UseCase 인터페이스 메서드 정의

--- src/main/java/dandi/dandi/comment/application/port/in/CommentUseCase.java
+++ src/main/java/dandi/dandi/comment/application/port/in/CommentUseCase.java
@@ -7,4 +7,6 @@ public interface CommentUseCase {
     void registerComment(Long memberId, Long postId, CommentRegisterCommand commentRegisterCommand);
 
     CommentResponses getComments(Long memberId, Long postId, Pageable pageable);
+
+    void deleteComment(Long memberId, Long commentId);
 }
