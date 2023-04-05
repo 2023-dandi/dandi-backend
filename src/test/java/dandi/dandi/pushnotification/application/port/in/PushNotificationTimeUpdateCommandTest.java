399cb33312550b8f06c82a5bcc844f4b011f4a67
Chris <bjuuuu98@gmail.com>
2023-04-05 14:01:37 +0900

    test: PushNotificationTimeUpdateCommandTest 상수 메시지 삭제

--- src/test/java/dandi/dandi/pushnotification/application/port/in/PushNotificationTimeUpdateCommandTest.java
+++ src/test/java/dandi/dandi/pushnotification/application/port/in/PushNotificationTimeUpdateCommandTest.java
@@ -10,16 +10,13 @@ import org.junit.jupiter.params.provider.NullSource;
 
 class PushNotificationTimeUpdateCommandTest {
 
-    private static final String NULL_PUSH_NOTIFICATION_TIME_EXCEPTION_MESSAGE = "푸시 알림 변경 시간이 존재하지 않습니다.";
-    private static final String INVALID_PUSH_NOTIFICATION_TIME_UNIT_EXCEPTION_MESSAGE = "푸시 알림 시간은 10분 단위입니다.";
-
     @DisplayName("null 값으로 PushNotificationTimeUpdateCommand을 생성하려하면 예외를 발생시킨다.")
     @ParameterizedTest
     @NullSource
     void create_Null(LocalTime pushNotificationTime) {
         assertThatThrownBy(() -> new PushNotificationTimeUpdateCommand(pushNotificationTime))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(NULL_PUSH_NOTIFICATION_TIME_EXCEPTION_MESSAGE);
+                .hasMessage("푸시 알림 변경 시간이 존재하지 않습니다.");
     }
 
     @DisplayName("10분 단위가 아닌 시각으로 PushNotificationTimeUpdateCommand을 생성하려하면 예외를 발생시킨다.")
@@ -31,6 +28,6 @@ class PushNotificationTimeUpdateCommandTest {
 
         assertThatThrownBy(() -> new PushNotificationTimeUpdateCommand(invalidPushNotificationTime))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(INVALID_PUSH_NOTIFICATION_TIME_UNIT_EXCEPTION_MESSAGE);
+                .hasMessage("푸시 알림 시간은 10분 단위입니다.");
     }
 }
