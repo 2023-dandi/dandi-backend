c36b5ddd275330f67c7d48fb644c8055f9190739
Chris <bjuuuu98@gmail.com>
2023-04-05 14:03:21 +0900

    test: PushNotificationAllowanceUpdateCommandTest 상수 메시지 삭제

--- src/test/java/dandi/dandi/pushnotification/application/port/in/PushNotificationAllowanceUpdateCommandTest.java
+++ src/test/java/dandi/dandi/pushnotification/application/port/in/PushNotificationAllowanceUpdateCommandTest.java
@@ -8,15 +8,12 @@ import org.junit.jupiter.params.provider.NullSource;
 
 class PushNotificationAllowanceUpdateCommandTest {
 
-    private static final String NULL_PUSH_NOTIFICATION_ALLOWANCE_UPDATE_VALUE_EXCEPTION_MESSAGE =
-            "푸시 알림 허용 여부가 입력되지 않았습니다.";
-
     @DisplayName("null 값으로 PushNotificationAllowanceUpdateCommand를 생성하려하면 예외를 발생시킨다.")
     @ParameterizedTest
     @NullSource
     void create_Null(Boolean allowance) {
         assertThatThrownBy(() -> new PushNotificationAllowanceUpdateCommand(allowance))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(NULL_PUSH_NOTIFICATION_ALLOWANCE_UPDATE_VALUE_EXCEPTION_MESSAGE);
+                .hasMessage("푸시 알림 허용 여부가 입력되지 않았습니다.");
     }
 }
