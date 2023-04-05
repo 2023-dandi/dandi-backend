d5adb1e0c66eb3e502187cc773a254711809104b
Chris <bjuuuu98@gmail.com>
2023-04-05 13:59:55 +0900

    refactor: NotFoundException 상수 메시지 삭제

--- src/main/java/dandi/dandi/common/exception/NotFoundException.java
+++ src/main/java/dandi/dandi/common/exception/NotFoundException.java
@@ -3,23 +3,20 @@ package dandi.dandi.common.exception;
 public class NotFoundException extends RuntimeException {
 
     private static final String NOT_FOUND_MESSAGE_FORMAT = "존재하지 않는 %s입니다.";
-    private static final String POST_RESOURCE_NAME = "게시글";
-    private static final String CLOTHES_RESOURCE_NAME = "옷";
-    private static final String COMMENT_RESOURCE_NAME = "댓글";
 
     private NotFoundException(String resourceName) {
         super(String.format(NOT_FOUND_MESSAGE_FORMAT, resourceName));
     }
 
     public static NotFoundException post() {
-        return new NotFoundException(POST_RESOURCE_NAME);
+        return new NotFoundException("게시글");
     }
 
     public static NotFoundException clothes() {
-        return new NotFoundException(CLOTHES_RESOURCE_NAME);
+        return new NotFoundException("옷");
     }
 
     public static NotFoundException comment() {
-        return new NotFoundException(COMMENT_RESOURCE_NAME);
+        return new NotFoundException("댓글");
     }
 }
