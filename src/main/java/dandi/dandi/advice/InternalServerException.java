7f760b6c260472e4d22a95a45eaf659628d1bbb8
Chris <bjuuuu98@gmail.com>
2023-04-08 00:25:18 +0900

    feat: 알림 조회시 발생할 수 있는 InternalServerException 정의

--- src/main/java/dandi/dandi/advice/InternalServerException.java
+++ src/main/java/dandi/dandi/advice/InternalServerException.java
@@ -20,4 +20,14 @@ public class InternalServerException extends RuntimeException {
         String exceptionMessage = String.format("탈퇴한 회원(memberId : %d)의 댓글 조회되었습니다.", memberId);
         return new InternalServerException(exceptionMessage);
     }
+
+    public static InternalServerException notificationCommentNotFound(Long commentId) {
+        String exceptionMessage = String.format("알림에 해당하는 댓글(commentId : %d)이 존재하지 않습니다.", commentId);
+        return new InternalServerException(exceptionMessage);
+    }
+
+    public static InternalServerException commentNotificationConvert(Long notificationId) {
+        String exceptionMessage = String.format("댓글 알림(notificationId : %d)은 도메인 매핑 실패", notificationId);
+        return new InternalServerException(exceptionMessage);
+    }
 }
