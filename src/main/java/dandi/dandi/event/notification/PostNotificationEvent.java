ebcfc397b7af92616a79d956d37c7d1d23009adc
Chris <bjuuuu98@gmail.com>
2023-04-07 14:34:10 +0900

    feat: 댓글 알림 이벤트에 해당하는 PostNotificationEvent의 정잭팩토리 정의

--- src/main/java/dandi/dandi/event/notification/PostNotificationEvent.java
+++ src/main/java/dandi/dandi/event/notification/PostNotificationEvent.java
@@ -6,16 +6,22 @@ public class PostNotificationEvent {
 
     private final Long targetMemberId;
     private final Long postId;
+    private final Long commentId;
     private final NotificationType notificationType;
 
-    public PostNotificationEvent(Long targetMemberId, Long postId, NotificationType notificationType) {
+    public PostNotificationEvent(Long targetMemberId, Long postId, Long commentId, NotificationType notificationType) {
         this.targetMemberId = targetMemberId;
         this.postId = postId;
+        this.commentId = commentId;
         this.notificationType = notificationType;
     }
 
     public static PostNotificationEvent postLike(Long targetMemberId, Long postId) {
-        return new PostNotificationEvent(targetMemberId, postId, NotificationType.POST_LIKE);
+        return new PostNotificationEvent(targetMemberId, postId, null, NotificationType.POST_LIKE);
+    }
+
+    public static PostNotificationEvent comment(Long targetMemberId, Long postId, Long commentId) {
+        return new PostNotificationEvent(targetMemberId, postId, commentId, NotificationType.COMMENT);
     }
 
     public Long getTargetMemberId() {
