b2ba827f39316174e11cebf7d6b13db8802a2b68
Chris <bjuuuu98@gmail.com>
2023-04-07 16:42:29 +0900

    feat: 게시글 댓글 알림 저장 기능 구현

---
+++ src/test/java/dandi/dandi/notification/application/CommentCreatedEventHandlerTest.java
@@ -0,0 +1,37 @@
+package dandi.dandi.notification.application;
+
+import static dandi.dandi.comment.CommentFixture.COMMENT_ID;
+import static dandi.dandi.member.MemberTestFixture.MEMBER_ID;
+import static dandi.dandi.post.PostFixture.POST_ID;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+import dandi.dandi.comment.domain.CommentCreatedEvent;
+import dandi.dandi.notification.application.port.out.NotificationPersistencePort;
+import dandi.dandi.notification.domain.Notification;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CommentCreatedEventHandlerTest {
+
+    @Mock
+    private NotificationPersistencePort notificationPersistencePort;
+
+    @InjectMocks
+    private CommentCreatedEventHandler commentCreatedEventHandler;
+
+    @DisplayName("게시글에 새 댓글 작성 알림을 저장할 수 있다.")
+    @Test
+    void handleCommentCreatedNotificationEvent() {
+        CommentCreatedEvent commentCreatedEvent = new CommentCreatedEvent(MEMBER_ID, POST_ID, COMMENT_ID);
+
+        commentCreatedEventHandler.handleCommentCreatedNotificationEvent(commentCreatedEvent);
+
+        verify(notificationPersistencePort).save(any(Notification.class));
+    }
+}
