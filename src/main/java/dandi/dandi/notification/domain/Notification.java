662a77ef6d148727332cd38a5aad1fcd3938469e
Chris <bjuuuu98@gmail.com>
2023-04-07 23:05:22 +0900

    refactor: Notification 객체에 createdAt 필드 삭제

--- src/main/java/dandi/dandi/notification/domain/Notification.java
+++ src/main/java/dandi/dandi/notification/domain/Notification.java
@@ -4,25 +4,22 @@ import static dandi.dandi.notification.domain.NotificationType.COMMENT;
 import static dandi.dandi.notification.domain.NotificationType.POST_LIKE;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 public class Notification {
 
     private final Long id;
     private final Long memberId;
     private final NotificationType type;
-    private final LocalDateTime createdAt;
     private final Long postId;
     private final Long commentId;
     private final Long commentContent;
     private final LocalDate weatherDate;
 
-    public Notification(Long id, Long memberId, NotificationType type, LocalDateTime createdAt, Long postId,
+    public Notification(Long id, Long memberId, NotificationType type, Long postId,
                         Long commentId, Long commentContent, LocalDate weatherDate) {
         this.id = id;
         this.memberId = memberId;
         this.type = type;
-        this.createdAt = createdAt;
         this.postId = postId;
         this.commentId = commentId;
         this.commentContent = commentContent;
@@ -30,15 +27,15 @@ public class Notification {
     }
 
     public static Notification postLike(Long targetMemberId, Long postId) {
-        return new Notification(null, targetMemberId, POST_LIKE, null, postId, null, null, null);
+        return new Notification(null, targetMemberId, POST_LIKE, null, postId, null, null);
     }
 
     public static Notification postComment(Long targetMemberId, Long postId, Long commentId) {
-        return new Notification(null, targetMemberId, COMMENT, null, postId, commentId, null, null);
+        return new Notification(null, targetMemberId, COMMENT, postId, commentId, null, null);
     }
 
     public static Notification whether(Long targetMemberId, LocalDate whetherDate) {
-        return new Notification(null, targetMemberId, null, null, null, null, null, whetherDate);
+        return new Notification(null, targetMemberId, null, null, null, null, whetherDate);
     }
 
     public Long getId() {
@@ -51,10 +48,6 @@ public class Notification {
 
     public NotificationType getType() {
         return type;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
     }
 
     public Long getPostId() {
@@ -79,7 +72,6 @@ public class Notification {
                 "id=" + id +
                 ", memberId=" + memberId +
                 ", type=" + type +
-                ", createdAt=" + createdAt +
                 ", postId=" + postId +
                 ", commentId=" + commentId +
                 ", commentContent=" + commentContent +
