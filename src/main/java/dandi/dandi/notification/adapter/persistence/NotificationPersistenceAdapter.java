5bf42a28fab43b04e7febe9eaa3464355d04fcd6
Chris <bjuuuu98@gmail.com>
2023-04-08 00:28:35 +0900

    feat: 알림 조회 기능 persistence 구현

--- src/main/java/dandi/dandi/notification/adapter/persistence/NotificationPersistenceAdapter.java
+++ src/main/java/dandi/dandi/notification/adapter/persistence/NotificationPersistenceAdapter.java
@@ -1,21 +1,54 @@
 package dandi.dandi.notification.adapter.persistence;
 
+import static dandi.dandi.notification.domain.NotificationType.COMMENT;
+
+import dandi.dandi.advice.InternalServerException;
+import dandi.dandi.comment.adapter.persistence.CommentJpaEntity;
+import dandi.dandi.comment.adapter.persistence.CommentRepository;
 import dandi.dandi.notification.application.port.out.NotificationPersistencePort;
 import dandi.dandi.notification.domain.Notification;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotificationPersistenceAdapter implements NotificationPersistencePort {
 
     private final NotificationRepository notificationRepository;
+    private final CommentRepository commentRepository;
 
-    public NotificationPersistenceAdapter(NotificationRepository notificationRepository) {
+    public NotificationPersistenceAdapter(NotificationRepository notificationRepository,
+                                          CommentRepository commentRepository) {
         this.notificationRepository = notificationRepository;
+        this.commentRepository = commentRepository;
     }
 
     @Override
     public void save(Notification notification) {
         NotificationJpaEntity notificationJpaEntity = NotificationJpaEntity.fromNotification(notification);
         notificationRepository.save(notificationJpaEntity);
+    }
+
+    @Override
+    public Slice<Notification> findByMemberId(Long memberId, Pageable pageable) {
+        Slice<NotificationJpaEntity> notificationJpaEntities =
+                notificationRepository.findByMemberId(memberId, pageable);
+        List<Notification> notifications = notificationJpaEntities.stream()
+                .map(this::mapToNotification)
+                .collect(Collectors.toUnmodifiableList());
+        return new SliceImpl<>(notifications, pageable, notificationJpaEntities.hasNext());
+    }
+
+    private Notification mapToNotification(NotificationJpaEntity notificationJpaEntity) {
+        if (notificationJpaEntity.hasNotificationType(COMMENT)) {
+            Long commentId = notificationJpaEntity.getCommentId();
+            CommentJpaEntity commentJpaEntity = commentRepository.findById(commentId)
+                    .orElseThrow(() -> InternalServerException.notificationCommentNotFound(commentId));
+            return notificationJpaEntity.toPostCommentNotification(commentJpaEntity.getContent());
+        }
+        return notificationJpaEntity.toNotification();
     }
 }
--- src/test/java/dandi/dandi/notification/adapter/persistence/NotificationPersistenceAdapterTest.java
+++ src/test/java/dandi/dandi/notification/adapter/persistence/NotificationPersistenceAdapterTest.java
@@ -1,20 +1,32 @@
 package dandi.dandi.notification.adapter.persistence;
 
+import static dandi.dandi.comment.CommentFixture.COMMENT_CONTENT;
 import static dandi.dandi.member.MemberTestFixture.MEMBER_ID;
 import static dandi.dandi.post.PostFixture.POST_ID;
+import static dandi.dandi.utils.PaginationUtils.CREATED_AT_DESC_TEST_SIZE_PAGEABLE;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
+import dandi.dandi.comment.adapter.persistence.CommentPersistenceAdapter;
+import dandi.dandi.comment.domain.Comment;
 import dandi.dandi.common.PersistenceAdapterTest;
 import dandi.dandi.notification.domain.Notification;
+import dandi.dandi.notification.domain.PostCommentNotification;
 import dandi.dandi.notification.domain.PostLikeNotification;
+import dandi.dandi.notification.domain.WeatherNotification;
+import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Slice;
 
 class NotificationPersistenceAdapterTest extends PersistenceAdapterTest {
 
     @Autowired
     private NotificationPersistenceAdapter notificationPersistenceAdapter;
+
+    @Autowired
+    private CommentPersistenceAdapter commentPersistenceAdapter;
 
     @DisplayName("알림을 저장할 수 있다.")
     @Test
@@ -23,5 +35,20 @@ class NotificationPersistenceAdapterTest extends PersistenceAdapterTest {
 
         assertThatCode(() -> notificationPersistenceAdapter.save(notification))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("사용자의 알림을 찾을 수 있다.")
+    @Test
+    void findByMemberId() {
+        Long commentId = commentPersistenceAdapter.save(Comment.initial(COMMENT_CONTENT), POST_ID, MEMBER_ID);
+        notificationPersistenceAdapter.save(PostCommentNotification.initial(MEMBER_ID, POST_ID, commentId));
+        notificationPersistenceAdapter.save(PostLikeNotification.initial(MEMBER_ID, POST_ID));
+        notificationPersistenceAdapter.save(PostLikeNotification.initial(MEMBER_ID, POST_ID));
+        notificationPersistenceAdapter.save(WeatherNotification.initial(MEMBER_ID, LocalDate.now()));
+
+        Slice<Notification> notifications =
+                notificationPersistenceAdapter.findByMemberId(MEMBER_ID, CREATED_AT_DESC_TEST_SIZE_PAGEABLE);
+
+        assertThat(notifications).hasSize(4);
     }
 }
