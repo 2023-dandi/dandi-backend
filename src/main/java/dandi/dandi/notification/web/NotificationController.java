bfd0020a0d945cd267c0870477e152c20c1b8207
Chris <bjuuuu98@gmail.com>
2023-04-09 00:49:17 +0900

    docs: 댓글 조회 기능 문서화

--- src/main/java/dandi/dandi/notification/web/NotificationController.java
+++ src/main/java/dandi/dandi/notification/web/NotificationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class NotificationController {
+public class NotificationController implements NotificationControllerDocs {
 
     private final NotificationUseCase notificationUseCase;
 
