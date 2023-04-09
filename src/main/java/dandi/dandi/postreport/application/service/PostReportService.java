c1418d0ebe489a60a1cfd730cbd7b5395de89e60
Chris <bjuuuu98@gmail.com>
2023-04-09 17:00:17 +0900

    feat: 이미 신고한 게시글을 신고하려고 하면 400 응답 기능 구현

--- src/main/java/dandi/dandi/postreport/application/service/PostReportService.java
+++ src/main/java/dandi/dandi/postreport/application/service/PostReportService.java
@@ -3,6 +3,7 @@ package dandi.dandi.postreport.application.service;
 import dandi.dandi.common.exception.NotFoundException;
 import dandi.dandi.post.application.port.out.PostPersistencePort;
 import dandi.dandi.postreport.application.port.in.PostReportUseCase;
+import dandi.dandi.postreport.application.port.out.PostReportPersistencePort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,20 +11,30 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostReportService implements PostReportUseCase {
 
     private final PostPersistencePort postPersistencePort;
+    private final PostReportPersistencePort postReportPersistencePort;
 
-    public PostReportService(PostPersistencePort postPersistencePort) {
+    public PostReportService(PostPersistencePort postPersistencePort,
+                             PostReportPersistencePort postReportPersistencePort) {
         this.postPersistencePort = postPersistencePort;
+        this.postReportPersistencePort = postReportPersistencePort;
     }
 
     @Override
     @Transactional
     public void reportPost(Long memberId, Long postId) {
         validatePostExistence(postId);
+        validateAlreadyReported(memberId, postId);
     }
 
     private void validatePostExistence(Long postId) {
         if (!postPersistencePort.existsById(postId)) {
             throw NotFoundException.post();
+        }
+    }
+
+    private void validateAlreadyReported(Long memberId, Long postId) {
+        if (postReportPersistencePort.existsByMemberIdAndPostId(memberId, postId)) {
+            throw new IllegalStateException("이미 신고한 게시글입니다.");
         }
     }
 }
--- src/test/java/dandi/dandi/postreport/application/service/PostReportServiceTest.java
+++ src/test/java/dandi/dandi/postreport/application/service/PostReportServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 
 import dandi.dandi.common.exception.NotFoundException;
 import dandi.dandi.post.application.port.out.PostPersistencePort;
+import dandi.dandi.postreport.application.port.out.PostReportPersistencePort;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,6 +21,9 @@ class PostReportServiceTest {
     @Mock
     private PostPersistencePort postPersistencePort;
 
+    @Mock
+    private PostReportPersistencePort postReportPersistencePort;
+
     @InjectMocks
     private PostReportService postReportService;
 
@@ -32,5 +36,18 @@ class PostReportServiceTest {
         assertThatThrownBy(() -> postReportService.reportPost(MEMBER_ID, POST_ID))
                 .isInstanceOf(NotFoundException.class)
                 .hasMessage(NotFoundException.post().getMessage());
+    }
+
+    @DisplayName("이미 신고한 글을 신고하려고 하면 예외를 발생시킨다.")
+    @Test
+    void reportPost_AlreadyReported() {
+        when(postPersistencePort.existsById(POST_ID))
+                .thenReturn(true);
+        when(postReportPersistencePort.existsByMemberIdAndPostId(MEMBER_ID, POST_ID))
+                .thenReturn(true);
+
+        assertThatThrownBy(() -> postReportService.reportPost(MEMBER_ID, POST_ID))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미 신고한 게시글입니다.");
     }
 }
