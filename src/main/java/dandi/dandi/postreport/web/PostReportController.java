b467ee76130ed8317409bd82e37a03fcbfdffb2e
Chris <bjuuuu98@gmail.com>
2023-04-09 17:11:44 +0900

    docs: 게시글 신고 기능 문서화

--- src/main/java/dandi/dandi/postreport/web/PostReportController.java
+++ src/main/java/dandi/dandi/postreport/web/PostReportController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PostReportController {
+public class PostReportController implements PostReportControllerDocs {
 
     private final PostReportUseCase postReportUseCase;
 
