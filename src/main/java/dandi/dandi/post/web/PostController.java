fb028ed0512206ca152e6648ad6a69790a2d50ea
Chris <bjuuuu98@gmail.com>
2023-04-11 00:05:24 +0900

    fix: PostController의 RequestMapping를 통한 request uri prefix 제거

--- src/main/java/dandi/dandi/post/web/PostController.java
+++ src/main/java/dandi/dandi/post/web/PostController.java
@@ -21,14 +21,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/posts")
 public class PostController implements PostControllerDocs {
 
     private final PostUseCase postUseCase;
