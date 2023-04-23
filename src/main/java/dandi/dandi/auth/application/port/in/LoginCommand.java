c95ac4fddd745697e0ab56bd16264cdaf0d57f52
Chris <bjuuuu98@gmail.com>
2023-04-23 22:17:38 +0900

    feat: 로그인/회원가입 요청시에 푸시 알림 토큰을 받도록 변경

--- src/main/java/dandi/dandi/auth/application/port/in/LoginCommand.java
+++ src/main/java/dandi/dandi/auth/application/port/in/LoginCommand.java
@@ -6,18 +6,28 @@ import javax.validation.constraints.NotNull;
 
 public class LoginCommand extends SelfValidating<LoginCommand> {
 
-    private static final String NULL_BLANK_LOGIN_COMMAND_EXCEPTION_MESSAGE = "idToken은 빈 문자열일 수 없습니다.";
+    private static final String NULL_BLANK_LOGIN_COMMAND_EXCEPTION_MESSAGE =
+            "idToken 혹은 pushNotificationToken은 빈 문자열일 수 없습니다.";
 
     @NotNull
     @NotBlank
     private final String idToken;
 
-    public LoginCommand(String idToken) {
+    @NotNull
+    @NotBlank
+    private final String pushNotificationToken;
+
+    public LoginCommand(String idToken, String pushNotificationToken) {
         this.idToken = idToken;
+        this.pushNotificationToken = pushNotificationToken;
         this.validateSelf(NULL_BLANK_LOGIN_COMMAND_EXCEPTION_MESSAGE);
     }
 
     public String getIdToken() {
         return idToken;
+    }
+
+    public String getPushNotificationToken() {
+        return pushNotificationToken;
     }
 }
