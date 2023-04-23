b424da4aff8056c323ddfa30dd5c91a6d70013d3
Chris <bjuuuu98@gmail.com>
2023-04-23 22:35:52 +0900

    test: PushNotification 테스트 객체 분리

--- src/test/java/dandi/dandi/pushnotification/PushNotificationFixture.java
+++ src/test/java/dandi/dandi/pushnotification/PushNotificationFixture.java
@@ -1,6 +1,12 @@
 package dandi.dandi.pushnotification;
 
+import dandi.dandi.pushnotification.domain.PushNotification;
+import dandi.dandi.pushnotification.domain.PushNotificationTime;
+import java.time.LocalTime;
+
 public class PushNotificationFixture {
 
     public static final String PUSH_NOTIFICATION_TOKEN = "asdasdsa";
+    public static final PushNotification PUSH_NOTIFICATION =
+            new PushNotification(1L, 1L, PUSH_NOTIFICATION_TOKEN, PushNotificationTime.from(LocalTime.MIDNIGHT), true);
 }
