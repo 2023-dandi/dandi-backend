606dc3ad14bca71fff7414cc7871d1d452519ac1
Chris <bjuuuu98@gmail.com>
2023-04-23 22:17:38 +0900

    feat: 로그인/회원가입 요청시에 푸시 알림 토큰을 받도록 변경

--- src/main/java/dandi/dandi/auth/web/in/LoginRequest.java
+++ src/main/java/dandi/dandi/auth/web/in/LoginRequest.java
@@ -1,24 +1,29 @@
 package dandi.dandi.auth.web.in;
 
 import dandi.dandi.auth.application.port.in.LoginCommand;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 public class LoginRequest {
 
     private String idToken;
+    private String pushNotificationToken;
 
     public LoginRequest() {
     }
 
-    public LoginRequest(String idToken) {
+    public LoginRequest(String idToken, String pushNotificationToken) {
         this.idToken = idToken;
+        this.pushNotificationToken = pushNotificationToken;
     }
 
     public String getIdToken() {
         return idToken;
     }
 
+    public String getPushNotificationToken() {
+        return pushNotificationToken;
+    }
+
     public LoginCommand toCommand() {
-        return new LoginCommand(idToken);
+        return new LoginCommand(idToken, pushNotificationToken);
     }
 }
