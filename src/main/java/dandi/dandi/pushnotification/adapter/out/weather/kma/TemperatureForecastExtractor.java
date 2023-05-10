f4b4f836a546d9a44668cd2591012ac45d5d0ce8
Chris <bjuuuu98@gmail.com>
2023-05-10 15:28:16 +0900

    refactor: 최저 최고 기온 추출 메서드 파라미터 변경

--- src/main/java/dandi/dandi/pushnotification/adapter/out/weather/kma/TemperatureForecastExtractor.java
+++ src/main/java/dandi/dandi/pushnotification/adapter/out/weather/kma/TemperatureForecastExtractor.java
@@ -12,45 +12,42 @@ public class TemperatureForecastExtractor {
     private static final String MAX_TEMPERATURE = "TMX";
     private static final String TEMPERATURE = "TMP";
 
-    public Temperature extract(String forecastDate, List<WeatherItem> weatherItems) {
-        int minTemperature = findMinTemperature(forecastDate, weatherItems);
-        int maxTemperature = findMaxTemperature(forecastDate, weatherItems);
+    public Temperature extract(List<WeatherItem> weatherItems) {
+        int minTemperature = findMinTemperature(weatherItems);
+        int maxTemperature = findMaxTemperature(weatherItems);
         return new Temperature(minTemperature, maxTemperature);
     }
 
-    private int findMinTemperature(String forecastDate, List<WeatherItem> weatherItems) {
+    private int findMinTemperature(List<WeatherItem> weatherItems) {
         return weatherItems.stream()
-                .filter(weatherItem -> weatherItem.getFcstDate().equals(forecastDate))
                 .filter(weatherItem -> weatherItem.getCategory().equals(MIN_TEMPERATURE))
                 .map(WeatherItem::getFcstValue)
                 .map(Integer::parseInt)
                 .findFirst()
-                .orElse(extractMinTemperature(forecastDate, weatherItems));
+                .orElse(extractMinTemperature(weatherItems));
     }
 
-    private int extractMinTemperature(String forecastDate, List<WeatherItem> weatherItems) {
-        return extractSortedTemperature(forecastDate, weatherItems).get(0);
+    private int extractMinTemperature(List<WeatherItem> weatherItems) {
+        return extractSortedTemperature(weatherItems).get(0);
     }
 
-    private int findMaxTemperature(String forecastDate, List<WeatherItem> weatherItems) {
+    private int findMaxTemperature(List<WeatherItem> weatherItems) {
         return weatherItems.stream()
-                .filter(weatherItem -> weatherItem.getFcstDate().equals(forecastDate))
                 .filter(weatherItem -> weatherItem.getCategory().equals(MAX_TEMPERATURE))
                 .map(WeatherItem::getFcstValue)
                 .map(Integer::parseInt)
                 .findFirst()
-                .orElse(extractMaxTemperature(forecastDate, weatherItems));
+                .orElse(extractMaxTemperature(weatherItems));
     }
 
-    private int extractMaxTemperature(String forecastDate, List<WeatherItem> weatherItems) {
-        List<Integer> sortedTemperatures = extractSortedTemperature(forecastDate, weatherItems);
+    private int extractMaxTemperature(List<WeatherItem> weatherItems) {
+        List<Integer> sortedTemperatures = extractSortedTemperature(weatherItems);
         return sortedTemperatures.get(sortedTemperatures.size() - 1);
     }
 
-    private List<Integer> extractSortedTemperature(String forecastDate, List<WeatherItem> weatherItems) {
+    private List<Integer> extractSortedTemperature(List<WeatherItem> weatherItems) {
         return weatherItems
                 .stream()
-                .filter(weather -> weather.getFcstDate().equals(forecastDate))
                 .filter(weather -> weather.getCategory().equals(TEMPERATURE))
                 .map(WeatherItem::getFcstValue)
                 .map(Integer::parseInt)
