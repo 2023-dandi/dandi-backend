a59b9c9e359b8ac5fb9bdebc9772c7883bc0a43e
Chris <bjuuuu98@gmail.com>
2023-05-11 11:43:24 +0900

    feat: 날씨 푸시 알림 스케쥴러 구현

---
+++ src/main/java/dandi/dandi/pushnotification/application/sevice/WeatherPushNotificationScheduler.java
@@ -0,0 +1,77 @@
+package dandi.dandi.pushnotification.application.sevice;
+
+import dandi.dandi.member.application.port.out.MemberPersistencePort;
+import dandi.dandi.member.domain.Location;
+import dandi.dandi.pushnotification.application.port.out.persistence.PushNotificationPersistencePort;
+import dandi.dandi.pushnotification.application.port.out.weather.WeatherForecast;
+import dandi.dandi.pushnotification.application.port.out.weather.WeatherForecastInfoManager;
+import dandi.dandi.pushnotification.application.port.out.webpush.WebPushManager;
+import dandi.dandi.pushnotification.application.sevice.message.WeatherPushNotificationMessageGenerator;
+import dandi.dandi.pushnotification.domain.PushNotification;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherPushNotificationScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(WeatherPushNotificationScheduler.class);
+    private static final int PUSH_NUMBER_UNIT = 10;
+    private static final String EVERY_SEVEN_AM = "0 0 7 * * *";
+
+    private final PushNotificationPersistencePort pushNotificationPersistencePort;
+    private final MemberPersistencePort memberPersistencePort;
+    private final WeatherForecastInfoManager weatherForecastInfoManager;
+    private final WeatherPushNotificationMessageGenerator weatherPushNotificationMessageGenerator;
+    private final WebPushManager webPushManager;
+
+    public WeatherPushNotificationScheduler(PushNotificationPersistencePort pushNotificationPersistencePort,
+                                            MemberPersistencePort memberPersistencePort,
+                                            WeatherForecastInfoManager weatherForecastInfoManager,
+                                            WeatherPushNotificationMessageGenerator weatherPushNotificationMessageGenerator,
+                                            WebPushManager webPushManager) {
+        this.pushNotificationPersistencePort = pushNotificationPersistencePort;
+        this.memberPersistencePort = memberPersistencePort;
+        this.weatherForecastInfoManager = weatherForecastInfoManager;
+        this.weatherPushNotificationMessageGenerator = weatherPushNotificationMessageGenerator;
+        this.webPushManager = webPushManager;
+    }
+
+    @Scheduled(cron = EVERY_SEVEN_AM)
+    public void sendPushWeatherNotification() {
+        Pageable pageable = PageRequest.of(0, PUSH_NUMBER_UNIT);
+        Slice<PushNotification> pushNotifications =
+                pushNotificationPersistencePort.findAllowedPushNotification(pageable);
+        pushWeatherNotification(pushNotifications.getContent());
+        while (pushNotifications.hasNext()) {
+            pageable = pageable.withPage(pageable.getPageNumber() + 1);
+            pushNotifications = pushNotificationPersistencePort.findAllowedPushNotification(pageable);
+            pushWeatherNotification(pushNotifications.getContent());
+        }
+    }
+
+    private void pushWeatherNotification(List<PushNotification> pushNotifications) {
+        for (PushNotification pushNotification : pushNotifications) {
+            Optional<Location> location = memberPersistencePort.findLocationById(pushNotification.getMemberId());
+            pushWeatherNotification(pushNotification, location);
+        }
+    }
+
+    private void pushWeatherNotification(PushNotification pushNotification, Optional<Location> location) {
+        if (location.isEmpty()) {
+            logger.info("탈퇴한 회원(memberId : {})의 푸시 알림 조회", pushNotification.getMemberId());
+            return;
+        }
+        WeatherForecast weatherForecast = weatherForecastInfoManager.getForecasts(LocalDate.now(), location.get());
+        String token = pushNotification.getToken();
+        String pushMessage = weatherPushNotificationMessageGenerator.generateMessage(weatherForecast);
+        webPushManager.pushMessage(token, pushMessage);
+    }
+}
---
+++ src/test/java/dandi/dandi/pushnotification/application/sevice/WeatherPushNotificationSchedulerTest.java
@@ -0,0 +1,102 @@
+package dandi.dandi.pushnotification.application.sevice;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import dandi.dandi.member.application.port.out.MemberPersistencePort;
+import dandi.dandi.member.domain.Location;
+import dandi.dandi.pushnotification.application.port.out.persistence.PushNotificationPersistencePort;
+import dandi.dandi.pushnotification.application.port.out.weather.WeatherForecast;
+import dandi.dandi.pushnotification.application.port.out.weather.WeatherForecastInfoManager;
+import dandi.dandi.pushnotification.application.port.out.webpush.WebPushManager;
+import dandi.dandi.pushnotification.application.sevice.message.WeatherPushNotificationMessageGenerator;
+import dandi.dandi.pushnotification.domain.PushNotification;
+import dandi.dandi.pushnotification.domain.PushNotificationTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.SliceImpl;
+
+@ExtendWith(MockitoExtension.class)
+class WeatherPushNotificationSchedulerTest {
+
+    @Mock
+    private PushNotificationPersistencePort pushNotificationPersistencePort;
+    @Mock
+    private MemberPersistencePort memberPersistencePort;
+    @Mock
+    private WeatherForecastInfoManager weatherForecastInfoManager;
+    @Mock
+    private WeatherPushNotificationMessageGenerator weatherPushNotificationMessageGenerator;
+    @Mock
+    private WebPushManager webPushManager;
+    @InjectMocks
+    private WeatherPushNotificationScheduler weatherPushNotificationScheduler;
+
+    @DisplayName("탈퇴한 회원에 대해 푸시 알림을 전송할수 없다.")
+    @Test
+    void sendPushWeatherNotification_withdrawnMember() {
+        Pageable pageable = PageRequest.of(0, 10);
+        PushNotification pushNotification =
+                new PushNotification(1L, 1L, "token1", PushNotificationTime.initial(), true);
+        when(pushNotificationPersistencePort.findAllowedPushNotification(pageable))
+                .thenReturn(new SliceImpl<>(List.of(pushNotification), pageable, false));
+        when(memberPersistencePort.findLocationById(1L))
+                .thenReturn(Optional.empty());
+
+        weatherPushNotificationScheduler.sendPushWeatherNotification();
+
+        assertAll(
+                () -> verify(weatherForecastInfoManager, never()).getForecasts(any(), any()),
+                () -> verify(weatherPushNotificationMessageGenerator, never()).generateMessage(any())
+        );
+    }
+
+    @DisplayName("푸시 알림을 전송할 수 있다.")
+    @Test
+    void sendPushWeatherNotification() {
+        Pageable firstPageable = PageRequest.of(0, 10);
+        Pageable secondPageable = PageRequest.of(1, 10);
+        Pageable thirdPageable = PageRequest.of(2, 10);
+        PushNotification pushNotification =
+                new PushNotification(1L, 1L, "token1", PushNotificationTime.initial(), true);
+        when(pushNotificationPersistencePort.findAllowedPushNotification(firstPageable))
+                .thenReturn(new SliceImpl<>(generatePushNotifications(), firstPageable, true));
+        when(pushNotificationPersistencePort.findAllowedPushNotification(secondPageable))
+                .thenReturn(new SliceImpl<>(generatePushNotifications(), secondPageable, true));
+        when(pushNotificationPersistencePort.findAllowedPushNotification(thirdPageable))
+                .thenReturn(new SliceImpl<>(List.of(pushNotification), thirdPageable, false));
+        when(memberPersistencePort.findLocationById(any()))
+                .thenReturn(Optional.of(new Location(10.0, 20.0)));
+        WeatherForecast weatherForecast = new WeatherForecast(10, 15);
+        when(weatherForecastInfoManager.getForecasts(any(), any()))
+                .thenReturn(weatherForecast);
+        when(weatherPushNotificationMessageGenerator.generateMessage(weatherForecast))
+                .thenReturn("푸시 알림 메시지");
+
+        weatherPushNotificationScheduler.sendPushWeatherNotification();
+
+        verify(webPushManager, times(21)).pushMessage(anyString(), anyString());
+    }
+
+    private List<PushNotification> generatePushNotifications() {
+        return LongStream.range(1, 11)
+                .mapToObj(index -> new PushNotification(
+                        index, index, "token" + index, PushNotificationTime.initial(), true))
+                .collect(Collectors.toUnmodifiableList());
+    }
+}
