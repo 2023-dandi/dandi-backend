89e42fc9e18270aeefdf98e1352814fda3cd7e39
Chris <bjuuuu98@gmail.com>
2023-05-15 00:58:46 +0900

    feat: 테스트 및 전시용 푸시 알림 등록 기능 구현

---
+++ src/main/java/dandi/dandi/pushnotification/application/service/PushNotificationSender.java
@@ -0,0 +1,55 @@
+package dandi.dandi.pushnotification.application.service;
+
+import dandi.dandi.advice.InternalServerException;
+import dandi.dandi.member.application.port.out.MemberPersistencePort;
+import dandi.dandi.member.domain.Location;
+import dandi.dandi.pushnotification.application.port.out.persistence.PushNotificationPersistencePort;
+import dandi.dandi.pushnotification.application.port.out.webpush.WebPushManager;
+import dandi.dandi.pushnotification.application.service.message.WeatherPushNotificationMessageGenerator;
+import dandi.dandi.pushnotification.domain.PushNotification;
+import dandi.dandi.weather.application.port.out.WeatherForecast;
+import dandi.dandi.weather.application.port.out.WeatherForecastInfoManager;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PushNotificationSender {
+
+    private final PushNotificationPersistencePort pushNotificationPersistencePort;
+    private final MemberPersistencePort memberPersistencePort;
+    private final WebPushManager webPushManager;
+    private final WeatherForecastInfoManager weatherForecastInfoManager;
+    private final WeatherPushNotificationMessageGenerator weatherPushNotificationMessageGenerator;
+    private final String weatherPushTitle;
+
+    public PushNotificationSender(PushNotificationPersistencePort pushNotificationPersistencePort,
+                                  MemberPersistencePort memberPersistencePort, WebPushManager webPushManager,
+                                  WeatherForecastInfoManager weatherForecastInfoManager,
+                                  WeatherPushNotificationMessageGenerator weatherPushNotificationMessageGenerator,
+                                  @Value("${weather.push.title}") String weatherPushTitle) {
+        this.pushNotificationPersistencePort = pushNotificationPersistencePort;
+        this.memberPersistencePort = memberPersistencePort;
+        this.weatherForecastInfoManager = weatherForecastInfoManager;
+        this.weatherPushNotificationMessageGenerator = weatherPushNotificationMessageGenerator;
+        this.webPushManager = webPushManager;
+        this.weatherPushTitle = weatherPushTitle;
+    }
+
+    @Transactional(readOnly = true)
+    public void pushWeatherNotification(Long memberId) {
+        PushNotification pushNotification = pushNotificationPersistencePort.findPushNotificationByMemberId(memberId)
+                .orElseThrow(() -> InternalServerException.pushNotificationNotFound(memberId));
+        if (pushNotification.isAllowed()) {
+            Location location = memberPersistencePort.findLocationById(memberId)
+                    .orElseThrow(() -> InternalServerException.withdrawnMemberPushNotification(memberId));
+            WeatherForecast weatherForecast = weatherForecastInfoManager.getForecasts(LocalDate.now(), location);
+            String pushMessageBody = weatherPushNotificationMessageGenerator.generateMessage(weatherForecast);
+            PushNotificationSource pushNotificationSource =
+                    new PushNotificationSource(pushNotification.getToken(), pushMessageBody);
+            webPushManager.pushMessages(weatherPushTitle, List.of(pushNotificationSource));
+        }
+    }
+}
