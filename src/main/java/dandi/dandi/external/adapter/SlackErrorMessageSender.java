afa23ad8363dda8d4bedcb8467a6ebd2ec79b98f
Chris <bjuuuu98@gmail.com>
2023-05-16 00:34:36 +0900

    feat: Slack 에러 메시지 전송시 비동기로 처리하도록 구현

--- src/main/java/dandi/dandi/external/adapter/SlackErrorMessageSender.java
+++ src/main/java/dandi/dandi/external/adapter/SlackErrorMessageSender.java
@@ -3,9 +3,11 @@ package dandi.dandi.external.adapter;
 import dandi.dandi.external.application.port.out.ErrorMessageSender;
 import net.gpedro.integrations.slack.SlackApi;
 import net.gpedro.integrations.slack.SlackMessage;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
+@Async(value = "errorMessageSendingAsyncExecutor")
 public class SlackErrorMessageSender implements ErrorMessageSender {
 
     private final SlackApi slackApi;
