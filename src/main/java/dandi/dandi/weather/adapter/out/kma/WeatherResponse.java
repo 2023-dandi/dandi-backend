63b18893b23d283286339890b8d12d5e3eb17f26
Chris <bjuuuu98@gmail.com>
2023-05-16 16:42:11 +0900

    feat: 기상청 응답 header DTO에 선언

--- src/main/java/dandi/dandi/weather/adapter/out/kma/WeatherResponse.java
+++ src/main/java/dandi/dandi/weather/adapter/out/kma/WeatherResponse.java
@@ -2,9 +2,14 @@ package dandi.dandi.weather.adapter.out.kma;
 
 public class WeatherResponse {
 
+    private WeatherResponseHeader header;
     private WeatherResponseBody body;
 
     public WeatherResponse() {
+    }
+
+    public WeatherResponseHeader getHeader() {
+        return header;
     }
 
     public WeatherResponseBody getBody() {
---
+++ src/main/java/dandi/dandi/weather/adapter/out/kma/WeatherResponseHeader.java
@@ -0,0 +1,18 @@
+package dandi.dandi.weather.adapter.out.kma;
+
+public class WeatherResponseHeader {
+
+    private String resultCode;
+    private String resultMsg;
+
+    public WeatherResponseHeader() {
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public String getResultMsg() {
+        return resultMsg;
+    }
+}
