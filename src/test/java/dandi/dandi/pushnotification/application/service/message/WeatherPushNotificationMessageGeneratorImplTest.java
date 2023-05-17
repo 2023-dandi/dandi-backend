39b4c5b8f988f5de35f479b3110658f972e32ca0
Chris <bjuuuu98@gmail.com>
2023-05-17 22:27:31 +0900

    feat: 날씨 정보 조회 결과 객체에 재시도 가능한 error 여부 필드 선언

--- src/test/java/dandi/dandi/pushnotification/application/service/message/WeatherPushNotificationMessageGeneratorImplTest.java
+++ src/test/java/dandi/dandi/pushnotification/application/service/message/WeatherPushNotificationMessageGeneratorImplTest.java
@@ -3,10 +3,11 @@ package dandi.dandi.pushnotification.application.service.message;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import dandi.dandi.weather.application.port.out.WeatherForecastResponse;
-import dandi.dandi.weather.application.port.out.WeatherForecastResultCode;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class WeatherPushNotificationMessageGeneratorImplTest {
 
@@ -15,15 +16,21 @@ class WeatherPushNotificationMessageGeneratorImplTest {
 
     @DisplayName("날씨 정보를 받아 날씨 푸시 알림 메시지를 생성한다.")
     @ParameterizedTest
-    @CsvSource({"SUCCESS_BUT_LOCATION_UPDATE, 오늘 날씨는 최저 10 / 최고 20입니다. 위치 정보를 수정해서 날씨 정확도를 높여주세요!",
-            "SUCCESS, 오늘 날씨는 최저 10 / 최고 20입니다. 단디에서 옷차림을 확인해보세요",
-            "FAILURE, 단디에서 최저 최고 기온에 따른 옷차림을 확인해보세요!"})
-    void generateMessage(WeatherForecastResultCode weatherForecastResultCode, String expectedMessage) {
-        WeatherForecastResponse weatherForecastResponse =
-                WeatherForecastResponse.ofSuccess(10, 20);
-
+    @MethodSource("provideWeatherForecastResponseAndExpectedMessage")
+    void generateMessage(WeatherForecastResponse weatherForecastResponse, String expectedMessage) {
         String pushMessage = weatherPushNotificationMessageGenerator.generateMessage(weatherForecastResponse);
 
         assertThat(pushMessage).isEqualTo(expectedMessage);
+    }
+
+    private static Stream<Arguments> provideWeatherForecastResponseAndExpectedMessage() {
+        return Stream.of(
+                Arguments.of(WeatherForecastResponse.ofSuccess(10, 20),
+                        "오늘 날씨는 최저 10 / 최고 20입니다. 단디에서 옷차림을 확인해보세요"),
+                Arguments.of(WeatherForecastResponse.ofSuccessButLocationUpdate(10, 20),
+                        "오늘 날씨는 최저 10 / 최고 20입니다. 위치 정보를 수정해서 날씨 정확도를 높여주세요!"),
+                Arguments.of(WeatherForecastResponse.ofFailure("DB_ERROR", true),
+                        "단디에서 최저 최고 기온에 따른 옷차림을 확인해보세요!")
+        );
     }
 }
