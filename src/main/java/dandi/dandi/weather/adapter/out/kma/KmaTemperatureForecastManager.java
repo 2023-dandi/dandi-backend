754773f89a1cad4c0f13214de265944d80ea5bdf
Chris <bjuuuu98@gmail.com>
2023-05-17 12:54:19 +0900

    feat: 기상청 api 응답코드에 따른 에러 처리 구현

--- src/main/java/dandi/dandi/weather/adapter/out/kma/KmaTemperatureForecastManager.java
+++ src/main/java/dandi/dandi/weather/adapter/out/kma/KmaTemperatureForecastManager.java
@@ -1,8 +1,14 @@
 package dandi.dandi.weather.adapter.out.kma;
 
 import dandi.dandi.member.domain.Location;
+import dandi.dandi.weather.adapter.out.kma.dto.TemperatureDto;
+import dandi.dandi.weather.adapter.out.kma.dto.WeatherItem;
+import dandi.dandi.weather.adapter.out.kma.dto.WeatherRequest;
+import dandi.dandi.weather.adapter.out.kma.dto.WeatherResponse;
+import dandi.dandi.weather.adapter.out.kma.dto.WeatherResponseBody;
 import dandi.dandi.weather.application.port.out.WeatherForecastInfoManager;
 import dandi.dandi.weather.application.port.out.WeatherForecastResponse;
+import dandi.dandi.weather.application.port.out.WeatherForecastResultCode;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -16,12 +22,15 @@ public class KmaTemperatureForecastManager implements WeatherForecastInfoManager
     private static final DateTimeFormatter KMA_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
     private static final String DATA_TYPE = "JSON";
     private static final String BASE_TIME = "0200";
+    private static final int DEFAULT_NX = 60;
+    private static final int DEFAULT_NY = 127;
     private static final int ROW_COUNT = 300;
 
     private final KmaWeatherApiCaller weatherApiCaller;
     private final String kmaServiceKey;
     private final KmaCoordinateConvertor kmaCoordinateConvertor;
     private final TemperatureForecastExtractor temperatureForecastExtractor;
+    private final WeatherRequest locationErrorHandleWeatherRequest;
 
     public KmaTemperatureForecastManager(KmaWeatherApiCaller weatherApiCaller,
                                          @Value("${weather.kma.service-key}") String kmaServiceKey,
@@ -31,25 +40,67 @@ public class KmaTemperatureForecastManager implements WeatherForecastInfoManager
         this.kmaServiceKey = kmaServiceKey;
         this.kmaCoordinateConvertor = kmaCoordinateConvertor;
         this.temperatureForecastExtractor = temperatureForecastExtractor;
+        this.locationErrorHandleWeatherRequest =
+                new WeatherRequest(kmaServiceKey, DATA_TYPE, BASE_TIME, ROW_COUNT, DEFAULT_NX, DEFAULT_NY);
     }
 
     public WeatherForecastResponse getForecasts(LocalDate now, Location location) {
         String baseDate = now.format(KMA_DATE_FORMATTER);
         Coordinate coordinate = kmaCoordinateConvertor.convert(location.getLatitude(), location.getLongitude());
-        WeatherRequest weatherRequest = new WeatherRequest(
+        WeatherRequest kmaWeatherRequest = new WeatherRequest(
                 kmaServiceKey, DATA_TYPE, baseDate, BASE_TIME, ROW_COUNT, coordinate.getNx(), coordinate.getNy());
-        List<WeatherItem> todayWeatherForecast = requestTodayWeatherForecast(baseDate, weatherRequest);
-        return temperatureForecastExtractor.extract(todayWeatherForecast);
+        return requestWeatherForecast(kmaWeatherRequest);
     }
 
-    private List<WeatherItem> requestTodayWeatherForecast(String baseDate, WeatherRequest weatherRequest) {
-        return weatherApiCaller.getWeathers(weatherRequest)
-                .getResponse()
-                .getBody()
-                .getItems()
+    private WeatherForecastResponse requestWeatherForecast(WeatherRequest weatherRequest) {
+        WeatherResponse weatherResponse = weatherApiCaller.getWeathers(weatherRequest)
+                .getResponse();
+        KmaResponseCode responseCode = extractResultCode(weatherResponse);
+        if (responseCode.isSuccessful()) {
+            return generateSuccessfulWeatherForecastResponse(
+                    WeatherForecastResultCode.SUCCESS, weatherRequest.getBase_date(), weatherResponse.getBody());
+        }
+        return retry(responseCode, weatherRequest);
+    }
+
+    private WeatherForecastResponse generateSuccessfulWeatherForecastResponse(WeatherForecastResultCode resultCode,
+                                                                              String baseDate,
+                                                                              WeatherResponseBody body) {
+        List<WeatherItem> temperatureForecasts = body.getItems()
                 .getItem()
                 .stream()
-                .filter(weatherItem -> weatherItem.getFcstDate().equals(baseDate))
+                .filter(kmaWeatherItem -> kmaWeatherItem.getFcstDate().equals(baseDate))
                 .collect(Collectors.toUnmodifiableList());
+        TemperatureDto temperature = temperatureForecastExtractor.extract(temperatureForecasts);
+        return new WeatherForecastResponse(
+                resultCode, temperature.getMinTemperature(), temperature.getMaxTemperature());
+    }
+
+    private WeatherForecastResponse retry(KmaResponseCode responseCode, WeatherRequest weatherRequest) {
+        if (responseCode.isRetryableNetworkError()) {
+            return retry(WeatherForecastResultCode.SUCCESS, weatherRequest);
+        }
+        if (responseCode.isErrorAssociatedWithLocation()) {
+            WeatherRequest defaultRetryRequest =
+                    locationErrorHandleWeatherRequest.ofBaseDate(weatherRequest.getBase_date());
+            return retry(WeatherForecastResultCode.SUCCESS_BUT_LOCATION_UPDATE, defaultRetryRequest);
+        }
+        return WeatherForecastResponse.ofFailure();
+    }
+
+    private WeatherForecastResponse retry(WeatherForecastResultCode resultCode, WeatherRequest weatherRequest) {
+        WeatherResponse weatherResponse = weatherApiCaller.getWeathers(weatherRequest)
+                .getResponse();
+        if (!extractResultCode(weatherResponse).isSuccessful()) {
+            return WeatherForecastResponse.ofFailure();
+        }
+        return generateSuccessfulWeatherForecastResponse(
+                resultCode, weatherRequest.getBase_date(), weatherResponse.getBody());
+    }
+
+    private KmaResponseCode extractResultCode(WeatherResponse kmaWeatherResponse) {
+        String resultCode = kmaWeatherResponse.getHeader()
+                .getResultCode();
+        return KmaResponseCode.from(resultCode);
     }
 }
---
+++ src/test/java/dandi/dandi/weather/adapter/out/kma/KmaTemperatureForecastManagerTest.java
@@ -0,0 +1,143 @@
+package dandi.dandi.weather.adapter.out.kma;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+import dandi.dandi.member.domain.Location;
+import dandi.dandi.weather.adapter.out.kma.dto.WeatherItem;
+import dandi.dandi.weather.adapter.out.kma.dto.WeatherItems;
+import dandi.dandi.weather.adapter.out.kma.dto.WeatherRequest;
+import dandi.dandi.weather.adapter.out.kma.dto.WeatherResponse;
+import dandi.dandi.weather.adapter.out.kma.dto.WeatherResponseBody;
+import dandi.dandi.weather.adapter.out.kma.dto.WeatherResponseHeader;
+import dandi.dandi.weather.adapter.out.kma.dto.WeatherResponses;
+import dandi.dandi.weather.application.port.out.WeatherForecastResponse;
+import dandi.dandi.weather.application.port.out.WeatherForecastResultCode;
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class KmaTemperatureForecastManagerTest {
+
+    private static final LocalDate NOW = LocalDate.of(2023, 5, 17);
+    private static final Location LOCATION = new Location(35.82795555555556, 128.53049722222224);
+    private static final WeatherRequest WEATHER_REQUEST = new WeatherRequest("serviceKey", "JSON", "20230517", "0200",
+            300, 88, 90);
+    private static final int MIN_TEMPERATURE = 10;
+    private static final int MAX_TEMPERATURE = 21;
+
+    private static final List<WeatherItem> weatherItems = List.of(
+            new WeatherItem("20230517", "0200", "TMX", "20230517", "1400", "21", "60", "127"),
+            new WeatherItem("20230517", "0200", "TMN", "20230517", "0500", "10", "60", "127"));
+    private static final WeatherResponses SUCCESS_WEATHER_RESPONSES = new WeatherResponses(new WeatherResponse(
+            new WeatherResponseHeader("00", "NORMAL_SERVICE"),
+            new WeatherResponseBody("JSON", new WeatherItems(weatherItems), 100, 1, 100))
+    );
+    private static final WeatherResponses UNKNOWN_ERROR_WEATHER_RESPONSES = new WeatherResponses(new WeatherResponse(
+            new WeatherResponseHeader("99", "UNKNOWN_ERROR"), new WeatherResponseBody()));
+    private static final WeatherResponses NETWORK_ERROR_WEATHER_RESPONSES = new WeatherResponses(new WeatherResponse(
+            new WeatherResponseHeader("05", "SERVICE_TIME_OUT"), new WeatherResponseBody()));
+    private static final WeatherResponses NO_DATA_ERROR_WEATHER_RESPONSES = new WeatherResponses(new WeatherResponse(
+            new WeatherResponseHeader("03", "NODATA_ERROR"), new WeatherResponseBody()));
+
+    private final KmaWeatherApiCaller weatherApiCaller = Mockito.mock(KmaWeatherApiCaller.class);
+    private final String serviceKey = "serviceKey";
+    private final KmaCoordinateConvertor kmaCoordinateConvertor = new KmaCoordinateConvertor();
+    private final TemperatureForecastExtractor temperatureForecastExtractor = new TemperatureForecastExtractor();
+
+    private final KmaTemperatureForecastManager kmaTemperatureForecastManager = new KmaTemperatureForecastManager(
+            weatherApiCaller, serviceKey, kmaCoordinateConvertor, temperatureForecastExtractor);
+
+
+    @DisplayName("기상청에서 날씨 정보를 정상적으로 받아올 수 있다.")
+    @Test
+    void getForecasts_Success() {
+        when(weatherApiCaller.getWeathers(WEATHER_REQUEST))
+                .thenReturn(SUCCESS_WEATHER_RESPONSES);
+
+        WeatherForecastResponse forecasts = kmaTemperatureForecastManager.getForecasts(NOW, LOCATION);
+
+        assertThat(forecasts.getResultCode()).isEqualTo(WeatherForecastResultCode.SUCCESS);
+    }
+
+    @DisplayName("기상청에서 재시도 할 수 없는 에러를 응답받는다면 FAILURE을 응답한다.")
+    @Test
+    void getForecasts_Failure() {
+        when(weatherApiCaller.getWeathers(WEATHER_REQUEST))
+                .thenReturn(UNKNOWN_ERROR_WEATHER_RESPONSES);
+
+        WeatherForecastResponse forecasts = kmaTemperatureForecastManager.getForecasts(NOW, LOCATION);
+
+        assertThat(forecasts.getResultCode()).isEqualTo(WeatherForecastResultCode.FAILURE);
+    }
+
+    @DisplayName("기상청에서 Service Timeout 응답을 받은 후 재시도를 통해 날씨 정보를 정상적으로 받아올 수 있다.")
+    @Test
+    void getForecasts_RetrySuccessAfterNetworkError() {
+        when(weatherApiCaller.getWeathers(WEATHER_REQUEST))
+                .thenReturn(NETWORK_ERROR_WEATHER_RESPONSES)
+                .thenReturn(SUCCESS_WEATHER_RESPONSES);
+
+        WeatherForecastResponse forecasts = kmaTemperatureForecastManager.getForecasts(NOW, LOCATION);
+
+        assertAll(
+                () -> assertThat(forecasts.getResultCode()).isEqualTo(WeatherForecastResultCode.SUCCESS),
+                () -> assertThat(forecasts.getMinTemperature()).isEqualTo(MIN_TEMPERATURE),
+                () -> assertThat(forecasts.getMaxTemperature()).isEqualTo(MAX_TEMPERATURE),
+                () -> Mockito.verify(weatherApiCaller, times(2)).getWeathers(WEATHER_REQUEST)
+        );
+    }
+
+    @DisplayName("기상청에서 Service Timeout 응답을 받은 후 재시도에 실패하면 Failure을 응답한다.")
+    @Test
+    void getForecasts_RetryFailureAfterNetworkError() {
+        when(weatherApiCaller.getWeathers(WEATHER_REQUEST))
+                .thenReturn(NETWORK_ERROR_WEATHER_RESPONSES);
+
+        WeatherForecastResponse forecasts = kmaTemperatureForecastManager.getForecasts(NOW, LOCATION);
+
+        assertThat(forecasts.getResultCode()).isEqualTo(WeatherForecastResultCode.FAILURE);
+    }
+
+    @DisplayName("기상청에서 위치 정보 오류에 의한 NODATA_ERROR 응답을 받은 후 기본 위치 정보 기반 재시도에 성공하면 SUCCESS_BUT_LOCATION_UPDATE를 응답한다.")
+    @Test
+    void getForecasts_RetrySuccessAfterNoDataError() {
+        when(weatherApiCaller.getWeathers(WEATHER_REQUEST))
+                .thenReturn(NO_DATA_ERROR_WEATHER_RESPONSES);
+        WeatherRequest locationErrorHandleWeatherRequest =
+                new WeatherRequest("serviceKey", "JSON", "20230517", "0200", 300, 60, 127);
+        when(weatherApiCaller.getWeathers(locationErrorHandleWeatherRequest))
+                .thenReturn(SUCCESS_WEATHER_RESPONSES);
+
+        WeatherForecastResponse forecasts = kmaTemperatureForecastManager.getForecasts(NOW, LOCATION);
+
+        assertAll(
+                () -> assertThat(forecasts.getResultCode())
+                        .isEqualTo(WeatherForecastResultCode.SUCCESS_BUT_LOCATION_UPDATE),
+                () -> assertThat(forecasts.getMinTemperature()).isEqualTo(MIN_TEMPERATURE),
+                () -> assertThat(forecasts.getMaxTemperature()).isEqualTo(MAX_TEMPERATURE)
+        );
+    }
+
+    @DisplayName("기상청에서 위치 정보 오류에 의한 NODATA_ERROR 응답을 받은 후 기본 위치 정보 기반 재시도에 실패하면 FAILURE을 응답한다.")
+    @Test
+    void getForecasts_RetryFailureAfterNoDataError() {
+        when(weatherApiCaller.getWeathers(WEATHER_REQUEST))
+                .thenReturn(NO_DATA_ERROR_WEATHER_RESPONSES);
+        WeatherRequest locationErrorHandleWeatherRequest =
+                new WeatherRequest("serviceKey", "JSON", "20230517", "0200", 300, 60, 127);
+        when(weatherApiCaller.getWeathers(locationErrorHandleWeatherRequest))
+                .thenReturn(UNKNOWN_ERROR_WEATHER_RESPONSES);
+
+        WeatherForecastResponse forecasts = kmaTemperatureForecastManager.getForecasts(NOW, LOCATION);
+
+        assertThat(forecasts.getResultCode()).isEqualTo(WeatherForecastResultCode.FAILURE);
+    }
+}
