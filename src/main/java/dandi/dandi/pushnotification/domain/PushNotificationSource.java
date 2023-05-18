67bee20637f1479940b3d187c2463e6b57fabde0
Chris <bjuuuu98@gmail.com>
2023-05-18 10:49:25 +0900

    feat: PushNotificationSource eq&hc 정의

--- src/main/java/dandi/dandi/pushnotification/domain/PushNotificationSource.java
+++ src/main/java/dandi/dandi/pushnotification/domain/PushNotificationSource.java
@@ -1,5 +1,7 @@
 package dandi.dandi.pushnotification.domain;
 
+import java.util.Objects;
+
 public class PushNotificationSource {
 
     private final String token;
@@ -16,5 +18,22 @@ public class PushNotificationSource {
 
     public String getBody() {
         return body;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof PushNotificationSource)) {
+            return false;
+        }
+        PushNotificationSource that = (PushNotificationSource) o;
+        return Objects.equals(token, that.token) && Objects.equals(body, that.body);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(token, body);
     }
 }
