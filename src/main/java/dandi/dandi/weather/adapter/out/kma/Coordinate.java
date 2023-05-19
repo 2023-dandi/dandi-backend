e064796c73e229e6da8d5c7fde6436d55536e341
Chris <bjuuuu98@gmail.com>
2023-05-19 11:25:27 +0900

    feat: Coordinate eq&hc 정의

--- src/main/java/dandi/dandi/weather/adapter/out/kma/Coordinate.java
+++ src/main/java/dandi/dandi/weather/adapter/out/kma/Coordinate.java
@@ -1,5 +1,7 @@
 package dandi.dandi.weather.adapter.out.kma;
 
+import java.util.Objects;
+
 public class Coordinate {
 
     private final int nx;
@@ -22,5 +24,22 @@ public class Coordinate {
         double maxDistanceInRange = Math.sqrt(Math.pow(range, 2) + Math.pow(range, 2));
         double distance = Math.sqrt(Math.pow(nx - another.nx, 2) + Math.pow(ny - another.ny, 2));
         return distance <= maxDistanceInRange;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Coordinate)) {
+            return false;
+        }
+        Coordinate that = (Coordinate) o;
+        return nx == that.nx && ny == that.ny;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nx, ny);
     }
 }
