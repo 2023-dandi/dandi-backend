6effef63ab3e6554b133915bf5832837fc5f9a38
Chris <bjuuuu98@gmail.com>
2023-05-20 23:54:59 +0900

    feat: LocationJpaEntity에 지역구 필드 선언

--- src/main/java/dandi/dandi/member/adapter/out/persistence/LocationJpaEntity.java
+++ src/main/java/dandi/dandi/member/adapter/out/persistence/LocationJpaEntity.java
@@ -1,5 +1,6 @@
 package dandi.dandi.member.adapter.out.persistence;
 
+import dandi.dandi.member.domain.District;
 import dandi.dandi.member.domain.Location;
 import javax.persistence.Embeddable;
 
@@ -8,16 +9,22 @@ public class LocationJpaEntity {
 
     private double latitude;
     private double longitude;
+    private String country;
+    private String city;
+    private String town;
 
     private LocationJpaEntity() {
     }
 
-    public LocationJpaEntity(double latitude, double longitude) {
+    public LocationJpaEntity(double latitude, double longitude, District district) {
         this.latitude = latitude;
         this.longitude = longitude;
+        this.country = district.getCountry();
+        this.city = district.getCity();
+        this.town = district.getTown();
     }
 
     public Location toLocation() {
-        return new Location(latitude, longitude);
+        return new Location(latitude, longitude, new District(country, city, town));
     }
 }
