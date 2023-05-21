11dc0ca7ad3ad3d7dc182af02b7224442d4a1bac
Chris <bjuuuu98@gmail.com>
2023-05-22 00:16:52 +0900

    docs: 전시회용 API 문서화

--- src/main/java/dandi/dandi/exhibition/ExhibitionController.java
+++ src/main/java/dandi/dandi/exhibition/ExhibitionController.java
@@ -1,5 +1,9 @@
 package dandi.dandi.exhibition;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //전시회 푸시 알림을 위한 임시 객체
+@Tag(name = "전시회용")
 @RestController
 @RequestMapping("/exhibition")
 public class ExhibitionController {
@@ -17,6 +22,11 @@ public class ExhibitionController {
         this.pushNotificationSender = pushNotificationSender;
     }
 
+    @Operation(summary = "현재 날씨 푸시 알림 발송")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "푸시 알림 발송 성공"),
+            @ApiResponse(responseCode = "400", description = "존재하지 않는 닉네임"),
+    })
     @PostMapping("/weather")
     public ResponseEntity<Void> pushWeatherNotification(@RequestBody ExhibitionPushNotificationRequest request) {
         pushNotificationSender.pushWeatherNotification(request.getNickname());
