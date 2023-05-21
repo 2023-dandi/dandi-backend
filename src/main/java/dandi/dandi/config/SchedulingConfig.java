caea9fefc2080f50d2aef04ffc3ec892ff55d1bf
Chris <bjuuuu98@gmail.com>
2023-05-21 22:59:26 +0900

    feat: EnableScheduling 어노테이션 추가

---
+++ src/main/java/dandi/dandi/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package dandi.dandi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
