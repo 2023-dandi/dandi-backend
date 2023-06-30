efd4d8ffdd37c1dbf149ae01dbefc609094e3c59
Chris <bjuuuu98@gmail.com>
2023-06-30 17:46:11 +0900

    refactor: clothes 패키지 응답 객체에 imageAccessUrl 추가하는 메서드 구현

--- src/main/java/dandi/dandi/clothes/application/port/in/ClothesResponse.java
+++ src/main/java/dandi/dandi/clothes/application/port/in/ClothesResponse.java
@@ -1,13 +1,24 @@
 package dandi.dandi.clothes.application.port.in;
 
-public class ClothesResponse {
+import dandi.dandi.clothes.domain.Clothes;
+import dandi.dandi.image.application.in.ImageResponse;
+
+public class ClothesResponse implements ImageResponse {
 
     private Long id;
     private String clothesImageUrl;
 
-    public ClothesResponse(Long id, String clothesImageUrl) {
+    public ClothesResponse() {
+    }
+
+    private ClothesResponse(Long id, String clothesImageUrl) {
         this.id = id;
         this.clothesImageUrl = clothesImageUrl;
+    }
+
+    public ClothesResponse(Clothes clothes) {
+        this.id = clothes.getId();
+        this.clothesImageUrl = clothes.getClothesImageUrl();
     }
 
     public Long getId() {
@@ -16,5 +27,10 @@ public class ClothesResponse {
 
     public String getClothesImageUrl() {
         return clothesImageUrl;
+    }
+
+    @Override
+    public ImageResponse addImageAccessUrl(String imageAccessUrl) {
+        return new ClothesResponse(id, clothesImageUrl);
     }
 }
