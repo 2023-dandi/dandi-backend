0fb130374d0cb86441b626c4ec622777ddde9a51
Chris <bjuuuu98@gmail.com>
2023-07-05 23:22:09 +0900

    test: imageAccessUrl 테스트 상수 수정

--- src/test/java/dandi/dandi/utils/TestImageUtils.java
+++ src/test/java/dandi/dandi/utils/TestImageUtils.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 public class TestImageUtils {
 
-    public static final String IMAGE_ACCESS_URL = "https://www.cloud-front.com/";
+    public static final String IMAGE_ACCESS_URL = "www.cloudFront.com/";
     public static final String TEST_IMAGE_RESOURCE_DIR = "/src/test/resources/static/images/";
     public static final String TEST_IMAGE_FILE_NAME = "test_img.jpg";
 
