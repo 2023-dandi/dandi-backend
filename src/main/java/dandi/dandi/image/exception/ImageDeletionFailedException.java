2a93bad55c8221bc674f66c2c1327bba1ba25a50
Chris <bjuuuu98@gmail.com>
2023-08-18 14:59:55 +0900

    refactor: 이미지 삭제 실패 예외 생성자 추가 및 예외 메시지 변경

--- src/main/java/dandi/dandi/image/exception/ImageDeletionFailedException.java
+++ src/main/java/dandi/dandi/image/exception/ImageDeletionFailedException.java
@@ -2,11 +2,19 @@ package dandi.dandi.image.exception;
 
 import dandi.dandi.advice.InternalServerException;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ImageDeletionFailedException extends InternalServerException {
 
-    private static final String MESSAGE = "이미지 파일 삭제에 실패했습니다.";
+    private static final String MESSAGE = "이미지 파일 삭제에 실패했습니다. \r\nfileKey ------%s------";
 
-    public ImageDeletionFailedException() {
-        super(MESSAGE);
+    public ImageDeletionFailedException(String fileName) {
+        super(String.format(MESSAGE, fileName));
+    }
+
+    public ImageDeletionFailedException(List<String> fileNames) {
+        this(fileNames.stream()
+                .collect(Collectors.joining("\r\n", "\r\n", "\r\n")));
     }
 }
