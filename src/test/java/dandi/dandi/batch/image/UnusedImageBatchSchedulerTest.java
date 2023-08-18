5953a0699042abe9a3bf9aa6d89fe89e57fc1f59
Chris <bjuuuu98@gmail.com>
2023-08-18 15:48:06 +0900

    feat: 미사용 이미지 Batch에 대한 스케쥴러 정의

---
+++ src/test/java/dandi/dandi/batch/image/UnusedImageBatchSchedulerTest.java
@@ -0,0 +1,43 @@
+package dandi.dandi.batch.image;
+
+import dandi.dandi.batch.exception.BatchException;
+import dandi.dandi.errormessage.application.port.out.ErrorMessageSender;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UnusedImageBatchSchedulerTest {
+
+    @Mock
+    private JobLauncher jobLauncher;
+    @Mock
+    private ErrorMessageSender errorMessageSender;
+    @InjectMocks
+    private UnusedImageBatchScheduler unusedImageBatchScheduler;
+
+    @Test
+    @DisplayName("미사용 이미지 삭제 Batch에 실패하면 관리자에게 메시지를 전송한다.")
+    void runUnusedImageDeletionBatch_Failure() throws JobInstanceAlreadyCompleteException, JobRestartException,
+            JobExecutionAlreadyRunningException, JobParametersInvalidException {
+        when(jobLauncher.run(any(), any()))
+                .thenThrow(BatchException.class);
+
+        unusedImageBatchScheduler.runUnusedImageDeletionBatch();
+
+        verify(errorMessageSender).sendMessage(anyString());
+    }
+}
