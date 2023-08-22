05fd44f1301ecb03cd48cacb84520567cc0a4d17
Chris <bjuuuu98@gmail.com>
2023-08-22 22:58:01 +0900

    feat: LocalDateTime 사용을 위한 커스텀 JobParameter 정의

---
+++ src/main/java/dandi/dandi/batch/jobparameter/DateTimeJobParameter.java
@@ -0,0 +1,23 @@
+package dandi.dandi.batch.jobparameter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeJobParameter {
+
+    private final LocalDateTime localDateTime;
+
+    public DateTimeJobParameter(String localDateTime) {
+        this.localDateTime = LocalDateTime.parse(localDateTime);
+    }
+
+    public LocalDate ofMinusDays(long days) {
+        return localDateTime.minusDays(days).toLocalDate();
+    }
+
+    @Override
+    public String toString() {
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+    }
+}
