22b9fce5a3a9045ccf754b6a5c58118c527185f6
Chris <bjuuuu98@gmail.com>
2023-08-25 16:34:45 +0900

    feat: 기상청 풍향 코드 enum 정의

---
+++ src/main/java/dandi/dandi/weather/adapter/out/kma/code/KmaWindDirection.java
@@ -0,0 +1,47 @@
+package dandi.dandi.weather.adapter.out.kma.code;
+
+import dandi.dandi.weather.adapter.out.kma.exception.KmaException;
+import dandi.dandi.weather.domain.WindDirection;
+
+import java.util.Arrays;
+
+public enum KmaWindDirection {
+
+    N(0, WindDirection.N),
+    NNE(1, WindDirection.NE),
+    NE(2, WindDirection.NE),
+    ENE(3, WindDirection.NE),
+    E(4, WindDirection.E),
+    ESE(5, WindDirection.SE),
+    SE(6, WindDirection.SE),
+    SSE(7, WindDirection.SE),
+    S(8, WindDirection.S),
+    SSW(9, WindDirection.SW),
+    SW(10, WindDirection.SE),
+    WSW(1, WindDirection.SW),
+    W(12, WindDirection.W),
+    WNW(13, WindDirection.NW),
+    NW(14, WindDirection.NW),
+    NNW(15, WindDirection.NW),
+    ;
+
+    private final int code;
+    private final WindDirection windDirection;
+
+    KmaWindDirection(int code, WindDirection windDirection) {
+        this.code = code;
+        this.windDirection = windDirection;
+    }
+
+    public static KmaWindDirection from(int value) {
+        int code = value % 16;
+        return Arrays.stream(values())
+                .filter(kmaWindDirection -> kmaWindDirection.code == code)
+                .findFirst()
+                .orElseThrow(KmaException::windDirectionCode);
+    }
+
+    public WindDirection getWindDirection() {
+        return windDirection;
+    }
+}
