6e1b24c3cc151a9c790cdf53308a724dfbaab89e
Chris <bjuuuu98@gmail.com>
2023-08-25 17:14:38 +0900

    feat: 기상청 날씨 데이터들을 받아 날씨 도메인으로 변환하는 객체 구현

---
+++ src/main/java/dandi/dandi/weather/adapter/out/kma/extractor/WeatherExtractors.java
@@ -0,0 +1,83 @@
+package dandi.dandi.weather.adapter.out.kma.extractor;
+
+import dandi.dandi.weather.adapter.out.kma.dto.WeatherItem;
+import dandi.dandi.weather.adapter.out.kma.exception.KmaException;
+import dandi.dandi.weather.domain.Weather;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static dandi.dandi.weather.adapter.out.kma.KmaConstant.KMA_DATE_FORMATTER;
+import static dandi.dandi.weather.adapter.out.kma.KmaConstant.KMA_TIME_FORMATTER;
+
+@Component
+public class WeatherExtractors {
+
+    private final List<WeatherExtractor> extractors;
+
+    public WeatherExtractors(List<WeatherExtractor> extractors) {
+        this.extractors = extractors;
+    }
+
+    public List<Weather> extract(List<WeatherItem> items, Long weatherLocationId) {
+        Map<FsctDateTimeKey, List<WeatherItem>> weatherItems = classifyByFsctDateTime(items);
+        return weatherItems.entrySet()
+                .stream()
+                .map(weatherItem -> generateWeather(weatherItem, weatherLocationId))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private Map<FsctDateTimeKey, List<WeatherItem>> classifyByFsctDateTime(List<WeatherItem> items) {
+        Map<FsctDateTimeKey, List<WeatherItem>> weatherItems = new HashMap<>();
+        for (WeatherItem item : items) {
+            FsctDateTimeKey key = new FsctDateTimeKey(item.getFcstDate(), item.getFcstTime());
+            weatherItems.computeIfAbsent(key, ignored -> new ArrayList<>())
+                    .add(item);
+        }
+        return weatherItems;
+    }
+
+    private Weather generateWeather(Map.Entry<FsctDateTimeKey, List<WeatherItem>> weatherItems, Long weatherLocationId) {
+        LocalDate date = LocalDate.parse(weatherItems.getKey().fsctDate, KMA_DATE_FORMATTER);
+        LocalTime time = LocalTime.parse(weatherItems.getKey().fsctTime, KMA_TIME_FORMATTER);
+        Weather.WeatherBuilder weatherBuilder = new Weather.WeatherBuilder(date, time, weatherLocationId);
+        for (WeatherItem item : weatherItems.getValue()) {
+            WeatherExtractor extractor = findExtractor(item.getCategory());
+            extractor.setValue(weatherBuilder, item.getFcstValue());
+        }
+        return weatherBuilder.build();
+    }
+
+    private WeatherExtractor findExtractor(String category) {
+        return extractors.stream()
+                .filter(weatherExtractor -> weatherExtractor.hasCategoryCode(category))
+                .findFirst()
+                .orElseThrow(KmaException::categoryCode);
+    }
+
+    static class FsctDateTimeKey {
+        private final String fsctDate;
+        private final String fsctTime;
+
+        public FsctDateTimeKey(String fsctDate, String fsctTime) {
+            this.fsctDate = fsctDate;
+            this.fsctTime = fsctTime;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            FsctDateTimeKey that = (FsctDateTimeKey) o;
+            return Objects.equals(fsctDate, that.fsctDate) && Objects.equals(fsctTime, that.fsctTime);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(fsctDate, fsctTime);
+        }
+    }
+}
