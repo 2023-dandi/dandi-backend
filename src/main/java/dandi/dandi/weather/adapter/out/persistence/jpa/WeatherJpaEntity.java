e56913d7b361a2540903eb3ce8735e1e2251f9ac
Chris <bjuuuu98@gmail.com>
2023-08-25 16:32:26 +0900

    feat: 날씨 엔티티 정의

---
+++ src/main/java/dandi/dandi/weather/adapter/out/persistence/jpa/WeatherJpaEntity.java
@@ -0,0 +1,55 @@
+package dandi.dandi.weather.adapter.out.persistence.jpa;
+
+import dandi.dandi.weather.domain.PrecipitationType;
+import dandi.dandi.weather.domain.Sky;
+import dandi.dandi.weather.domain.Weather;
+import dandi.dandi.weather.domain.WindDirection;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "weather")
+public class WeatherJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "weather_id")
+    private Long id;
+
+    private long weatherLocationId;
+    private LocalDate date;
+    private LocalTime time;
+    private double temperature;
+
+    @Enumerated(value = EnumType.STRING)
+    private Sky sky;
+
+    private int humidity;
+
+    @Enumerated(value = EnumType.STRING)
+    private PrecipitationType precipitationType;
+    private int precipitationPossibility;
+    private double precipitationAmount;
+
+    @Enumerated(value = EnumType.STRING)
+    private WindDirection windDirection;
+    private double windSpeed;
+
+    protected WeatherJpaEntity() {
+    }
+
+    public Weather toWeather() {
+        return new Weather.WeatherBuilder(date, time, weatherLocationId)
+                .temperature(temperature)
+                .sky(sky)
+                .humidity(humidity)
+                .precipitationType(precipitationType)
+                .precipitationPossibility(precipitationPossibility)
+                .precipitationAmount(precipitationAmount)
+                .windDirection(windDirection)
+                .windSpeed(windSpeed)
+                .build();
+    }
+}
