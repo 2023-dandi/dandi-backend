390b1ca0e0bf9af0f7f118afa777a11fc3bb2a8a
Chris <bjuuuu98@gmail.com>
2023-08-28 15:35:40 +0900

    feat: KmaResponseCode에 일시적 에러인지에 대한 메서드 추가

--- src/main/java/dandi/dandi/weather/adapter/out/kma/code/KmaResponseCode.java
+++ src/main/java/dandi/dandi/weather/adapter/out/kma/code/KmaResponseCode.java
@@ -45,6 +45,12 @@ public enum KmaResponseCode {
         return this == NO_DATA_ERROR;
     }
 
+    public boolean isTemporaryExternalServerError() {
+        return this == SERVICE_TIME_OUT || this == DB_ERROR || this == HTTP_ERROR ||
+                this == SERVICE_ACCESS_DENIED_ERROR || this == TEMPORARILY_DISABLE_THE_SERVICE_KEY_ERROR ||
+                this == SERVICE_KEY_IS_NOT_REGISTERED_ERROR;
+    }
+
     public boolean isRetryable() {
         return this == SERVICE_TIME_OUT || this == DB_ERROR || this == HTTP_ERROR ||
                 this == SERVICE_ACCESS_DENIED_ERROR || this == TEMPORARILY_DISABLE_THE_SERVICE_KEY_ERROR ||
