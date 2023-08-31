8580b73aa658442f105c972b5d2ee55d123c2322
Chris <bjuuuu98@gmail.com>
2023-08-31 19:59:46 +0900

    feat: 기상청에서 날씨를 받아오는 Batch 구현

---
+++ src/main/java/dandi/dandi/batch/weather/WeatherBatch.java
@@ -0,0 +1,143 @@
+package dandi.dandi.batch.weather;
+
+import dandi.dandi.batch.exception.BatchException;
+import dandi.dandi.batch.jobparameter.DateTimeJobParameter;
+import dandi.dandi.weather.application.port.out.WeatherPersistencePort;
+import dandi.dandi.weather.application.port.out.WeatherRequestFatalException;
+import dandi.dandi.weather.application.port.out.WeatherRequestRetryableException;
+import dandi.dandi.weather.application.port.out.WeatherRequester;
+import dandi.dandi.weather.domain.WeatherLocation;
+import dandi.dandi.weather.domain.Weathers;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+public class WeatherBatch {
+
+    private static final String JOB_NAME = "weatherBatchJob";
+    private static final int CHUCK_SIZE = 1000;
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+    private final WeatherRequester weatherRequester;
+    private final DateTimeJobParameter dateTimeJobParameter;
+    private final WeatherPersistencePort weatherPersistencePort;
+
+    public WeatherBatch(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory,
+                        DataSource dataSource, WeatherRequester weatherRequester,
+                        @Qualifier(value = "weatherJobDateTimeParameter") DateTimeJobParameter dateTimeJobParameter, WeatherPersistencePort weatherPersistencePort) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.dataSource = dataSource;
+        this.weatherRequester = weatherRequester;
+        this.dateTimeJobParameter = dateTimeJobParameter;
+        this.weatherPersistencePort = weatherPersistencePort;
+    }
+
+    @Bean(name = "weatherJobDateTimeParameter")
+    @JobScope
+    public DateTimeJobParameter jobParameter(@Value("#{jobParameters[dateTime]}") String dateTime) {
+        return new DateTimeJobParameter(dateTime);
+    }
+
+    @Bean(JOB_NAME)
+    public Job weatherBatch() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(weatherBatchStep(null))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step weatherBatchStep(@Value("#{jobParameters[backOffPeriod]}") Long backOffPeriod) {
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(backOffPeriod);
+        return stepBuilderFactory.get("weatherInsertion")
+                .<WeatherLocation, Weathers>chunk(CHUCK_SIZE)
+                .reader(weatherLocationItemReader())
+                .processor(weatherItemProcessor())
+                .writer(weatherItemWriter())
+                .faultTolerant()
+                .noRetry(WeatherRequestFatalException.class)
+                .retry(WeatherRequestRetryableException.class)
+                .retryLimit(2)
+                .backOffPolicy(fixedBackOffPolicy)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ItemReader<WeatherLocation> weatherLocationItemReader() {
+        return new JdbcPagingItemReaderBuilder<WeatherLocation>()
+                .name("unusedImageItemReader")
+                .dataSource(dataSource)
+                .pageSize(CHUCK_SIZE)
+                .fetchSize(CHUCK_SIZE)
+                .queryProvider(pagingQueryProvider())
+                .rowMapper(rowMapper())
+                .build();
+    }
+
+    private PagingQueryProvider pagingQueryProvider() {
+        SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+        queryProvider.setDataSource(dataSource);
+        queryProvider.setSelectClause("weather_location_id, latitude, longitude, first_district, second_district, third_district");
+        queryProvider.setFromClause("from weather_location");
+        queryProvider.setSortKey("weather_location_id");
+        try {
+            return queryProvider.getObject();
+        } catch (Exception e) {
+            throw BatchException.queryProviderObjectCreationFailed(JOB_NAME);
+        }
+    }
+
+    private RowMapper<WeatherLocation> rowMapper() {
+        return (rs, rowNum) -> new WeatherLocation(
+                rs.getLong("weather_location_id"),
+                rs.getDouble("latitude"),
+                rs.getDouble("longitude"),
+                rs.getString("first_district"),
+                rs.getString("second_district"),
+                rs.getString("third_district")
+        );
+    }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<WeatherLocation, Weathers> weatherItemProcessor() {
+        weatherRequester.finish();
+        return weatherLocation -> weatherRequester.getWeathers(dateTimeJobParameter.getLocalDateTime(), weatherLocation);
+    }
+
+    @Bean
+    @StepScope
+    public ItemWriter<Weathers> weatherItemWriter() {
+        return items -> {
+            List<Weathers> weathers = items.stream()
+                    .map(item -> (Weathers) item)
+                    .collect(Collectors.toUnmodifiableList());
+            weatherPersistencePort.save(weathers);
+        };
+    }
+}
---
+++ src/test/java/dandi/dandi/batch/weather/WeatherBatchTest.java
@@ -0,0 +1,138 @@
+package dandi.dandi.batch.weather;
+
+import dandi.dandi.common.DatabaseCleaner;
+import dandi.dandi.weather.adapter.out.persistence.jpa.WeatherJpaEntity;
+import dandi.dandi.weather.adapter.out.persistence.jpa.WeatherLocationJpaEntity;
+import dandi.dandi.weather.adapter.out.persistence.jpa.WeatherPersistenceAdapter;
+import dandi.dandi.weather.adapter.out.persistence.jpa.WeatherRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@RunWith(SpringRunner.class)
+@SpringBatchTest
+@EnableAutoConfiguration
+@EnableBatchProcessing
+@EnableJpaRepositories(basePackageClasses = {WeatherRepository.class})
+@EntityScan(basePackageClasses = {WeatherJpaEntity.class, WeatherLocationJpaEntity.class})
+@SpringBootTest(classes = {WeatherBatch.class, WeatherPersistenceAdapter.class, WeatherBatchTestConfig.class, DatabaseCleaner.class})
+class WeatherBatchTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private WeatherRepository weatherRepository;
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @AfterEach
+    void clearDatabase() {
+        databaseCleaner.truncate();
+        databaseCleaner.initializeAutoIncrement();
+    }
+
+    @DisplayName("날씨 위치에 기반한 날씨 정보를 받아와 저장할 수 있다.")
+    @Test
+    void weatherBatchJob() throws Exception {
+        LocalDateTime now = LocalDateTime.now().plusDays(1);
+        JobParameters jobParameters = new JobParameters(Map.of("dateTime", new JobParameter(now.toString()), "backOffPeriod", new JobParameter(1L)));
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(new WeatherLocationJpaEntity(null, 110, 100, "서울특별시", null, null));
+        entityManager.getTransaction().commit();
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        assertAll(
+                () -> assertThat(jobExecution.getExitStatus()).isEqualTo(ExitStatus.COMPLETED),
+                () -> assertThat(weatherRepository.findAll()).hasSize(24)
+        );
+    }
+
+    @DisplayName("날씨 위치에 기반한 날씨 정보를 받아오는 도중 WeatherRequestRetryableException이 발생하면 재시도 한다.")
+    @Test
+    void weatherBatchJob_RetryableException_Success() throws Exception {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime dateTime = LocalDateTime.of(
+                2021, now.getMonth(), now.getDayOfMonth(), now.getHour(), now.getMinute(), now.getSecond());
+        JobParameters jobParameters = new JobParameters(Map.of("dateTime", new JobParameter(dateTime.toString()), "backOffPeriod", new JobParameter(1L)));
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(new WeatherLocationJpaEntity(null, 110, 100, "서울특별시", null, null));
+        entityManager.getTransaction().commit();
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        assertAll(
+                () -> assertThat(jobExecution.getExitStatus()).isEqualTo(ExitStatus.COMPLETED),
+                () -> assertThat(weatherRepository.findAll()).hasSize(24)
+        );
+    }
+
+    @DisplayName("날씨 위치에 기반한 날씨 정보를 받아오는 도중 WeatherRequestRetryableException이 발생하고 1회의 재시도도 실패하면 재시도 하지 않는다.")
+    @Test
+    void weatherBatchJob_RetryableException_Failed() throws Exception {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime dateTime = LocalDateTime.of(
+                2022, now.getMonth(), now.getDayOfMonth(), now.getHour(), now.getMinute(), now.getSecond());
+        JobParameters jobParameters = new JobParameters(Map.of("dateTime", new JobParameter(dateTime.toString()), "backOffPeriod", new JobParameter(1L)));
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(new WeatherLocationJpaEntity(null, 110, 100, "서울특별시", null, null));
+        entityManager.getTransaction().commit();
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        assertAll(
+                () -> assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo(ExitStatus.FAILED.getExitCode()),
+                () -> assertThat(weatherRepository.findAll()).hasSize(0)
+        );
+    }
+
+    @DisplayName("날씨 위치에 기반한 날씨 정보를 받아오는 도중 WeatherRequestFatalException이 발생하고 실패한다.")
+    @Test
+    void weatherBatchJob_FatalException_Failed() throws Exception {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime dateTime = LocalDateTime.of(
+                2020, now.getMonth(), now.getDayOfMonth(), now.getHour(), now.getMinute(), now.getSecond());
+        JobParameters jobParameters = new JobParameters(Map.of("dateTime", new JobParameter(dateTime.toString()), "backOffPeriod", new JobParameter(1L)));
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(new WeatherLocationJpaEntity(null, 110, 100, "서울특별시", null, null));
+        entityManager.getTransaction().commit();
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        assertAll(
+                () -> assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo(ExitStatus.FAILED.getExitCode()),
+                () -> assertThat(weatherRepository.findAll()).hasSize(0)
+        );
+    }
+}
