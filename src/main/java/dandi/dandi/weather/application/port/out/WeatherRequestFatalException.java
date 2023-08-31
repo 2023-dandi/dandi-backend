ec259661927655759d5bd0f9d3a6e5b86310cbe6
Chris <bjuuuu98@gmail.com>
2023-08-31 15:35:50 +0900

    refactor: WeatherRequestFatalException의 noData 메세지 소수점 자릿수 증가

--- src/main/java/dandi/dandi/weather/application/port/out/WeatherRequestFatalException.java
+++ src/main/java/dandi/dandi/weather/application/port/out/WeatherRequestFatalException.java
@@ -2,7 +2,7 @@ package dandi.dandi.weather.application.port.out;
 
 public class WeatherRequestFatalException extends WeatherRequestException {
 
-    private static final String NO_DATA_EXCEPTION_MESSAGE = "위치에 대한 날씨 정보가 존재하지 않습니다.(%f, %f)";
+    private static final String NO_DATA_EXCEPTION_MESSAGE = "위치에 대한 날씨 정보가 존재하지 않습니다.(%.15f, %.15f)";
 
     public WeatherRequestFatalException(String message) {
         super(message);
