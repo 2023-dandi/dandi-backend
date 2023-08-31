f6e4a35dd5a20cd6f7f8b55fa8e1b05cf54cb63b
Chris <bjuuuu98@gmail.com>
2023-08-31 15:42:46 +0900

    feat: 날씨 정보를 얻어오는 out-port 정의

---
+++ src/main/java/dandi/dandi/weather/application/port/out/WeatherRequester.java
@@ -0,0 +1,13 @@
+package dandi.dandi.weather.application.port.out;
+
+import dandi.dandi.weather.domain.WeatherLocation;
+import dandi.dandi.weather.domain.Weathers;
+
+import java.time.LocalDateTime;
+
+public interface WeatherRequester {
+
+    Weathers getWeathers(LocalDateTime now, WeatherLocation location) throws WeatherRequestException;
+
+    void finish();
+}
