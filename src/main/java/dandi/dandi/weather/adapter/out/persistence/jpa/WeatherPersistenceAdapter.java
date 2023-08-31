0c34fdab39f6e62d68703fff1c917fbbdd4e47e5
Chris <bjuuuu98@gmail.com>
2023-08-31 15:45:27 +0900

    feat: WeatherPersistenceAdapter에 날씨 저장 기능 구현

---
+++ src/main/java/dandi/dandi/weather/adapter/out/persistence/jpa/WeatherPersistenceAdapter.java
@@ -0,0 +1,36 @@
+package dandi.dandi.weather.adapter.out.persistence.jpa;
+
+import dandi.dandi.weather.application.port.out.WeatherPersistencePort;
+import dandi.dandi.weather.domain.Weathers;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class WeatherPersistenceAdapter implements WeatherPersistencePort {
+
+    private final WeatherRepository weatherRepository;
+
+    public WeatherPersistenceAdapter(WeatherRepository weatherRepository) {
+        this.weatherRepository = weatherRepository;
+    }
+
+    @Override
+    public void save(List<Weathers> weathers) {
+        List<WeatherJpaEntity> weatherJpaEntities = weathers.stream()
+                .map(this::parseToEntity)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toUnmodifiableList());
+        weatherRepository.saveAll(weatherJpaEntities);
+    }
+
+    private List<WeatherJpaEntity> parseToEntity(Weathers weathers) {
+        long weatherLocationId = weathers.getWeatherLocationId();
+        return weathers.getValues()
+                .stream()
+                .map(weather -> WeatherJpaEntity.ofWeather(weather, weatherLocationId))
+                .collect(Collectors.toUnmodifiableList());
+    }
+}
