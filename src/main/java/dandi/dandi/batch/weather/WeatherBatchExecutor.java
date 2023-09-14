d40522ebd86ffd0ee1c02a83930d3093b573955f
Chris <bjuuuu98@gmail.com>
2023-09-14 16:54:41 +0900

    feat: WeatherBatchExecutor 선언

---
+++ src/main/java/dandi/dandi/batch/weather/WeatherBatchExecutor.java
@@ -0,0 +1,59 @@
+package dandi.dandi.batch.weather;
+
+import dandi.dandi.batch.exception.BatchException;
+import dandi.dandi.errormessage.application.port.out.ErrorMessageSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.springframework.batch.core.ExitStatus.FAILED;
+
+@Component
+public class WeatherBatchExecutor {
+
+    private static final Logger logger = LoggerFactory.getLogger("asyncLogger");
+    private static final long FIVE_MINUTES = 5000000L;
+
+    private final JobLauncher jobLauncher;
+    private final WeatherBatch weatherBatch;
+    private final ErrorMessageSender errorMessageSender;
+
+    public WeatherBatchExecutor(JobLauncher jobLauncher, WeatherBatch weatherBatch, ErrorMessageSender errorMessageSender) {
+        this.jobLauncher = jobLauncher;
+        this.weatherBatch = weatherBatch;
+        this.errorMessageSender = errorMessageSender;
+    }
+
+    public void runWeatherBatch() {
+        String now = LocalDateTime.now().toString();
+        JobParameters jobParameters = new JobParameters(
+                Map.of(
+                        "dateTime", new JobParameter(now),
+                        "backOffPeriod", new JobParameter(FIVE_MINUTES)
+                )
+        );
+        try {
+            JobExecution jobExecution = jobLauncher.run(weatherBatch.weatherBatch(), jobParameters);
+            handleFailureIfFailed(now, jobExecution.getExitStatus());
+        } catch (BatchException | JobExecutionAlreadyRunningException | JobRestartException |
+                 JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {
+            errorMessageSender.sendMessage(now + " Weather Batch Failed");
+            logger.info("Weather Batch Failed \r\n {}", e.getMessage());
+        }
+    }
+
+    private void handleFailureIfFailed(String now, ExitStatus exitStatus) {
+        if (exitStatus.getExitCode().equals(FAILED.getExitCode())) {
+            errorMessageSender.sendMessage(now + " Weather Batch Failed");
+            logger.info("Weather Batch Failed \r\n {}", exitStatus.getExitDescription());
+        }
+    }
+}
---
+++ src/test/java/dandi/dandi/batch/weather/WeatherBatchExecutorTest.java
@@ -0,0 +1,63 @@
+package dandi.dandi.batch.weather;
+
+import dandi.dandi.batch.exception.BatchException;
+import dandi.dandi.errormessage.application.port.out.ErrorMessageSender;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class WeatherBatchExecutorTest {
+
+    @Mock
+    private JobLauncher jobLauncher;
+    @Mock
+    private ErrorMessageSender errorMessageSender;
+    @Mock
+    private WeatherBatch weatherBatch;
+    @InjectMocks
+    private WeatherBatchExecutor weatherBatchExecutor;
+
+    @DisplayName("날씨 Batch 작업 중 Batch 관련 예외가 발생하면 관리자에게 메시지를 전송한다.")
+    @Test
+    void runWeatherBatch_BatchException() throws JobInstanceAlreadyCompleteException,
+            JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        when(jobLauncher.run(any(), any()))
+                .thenThrow(BatchException.class);
+
+        weatherBatchExecutor.runWeatherBatch();
+
+        verify(errorMessageSender).sendMessage(anyString());
+    }
+
+    @DisplayName("날씨 Batch가 실패로 종료되면 관리자에게 메시지를 전송한다.")
+    @Test
+    void runWeatherBatch_FailedExitStatus() throws JobInstanceAlreadyCompleteException,
+            JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        JobExecution jobExecution = Mockito.mock(JobExecution.class);
+        when(jobLauncher.run(any(), any()))
+                .thenReturn(jobExecution);
+        when(jobExecution.getExitStatus())
+                .thenReturn(ExitStatus.FAILED);
+
+        weatherBatchExecutor.runWeatherBatch();
+
+        verify(errorMessageSender).sendMessage(anyString());
+    }
+}
