e38a9f84d74062b3dd0fc975c118d8dd6b9929be
Chris <bjuuuu98@gmail.com>
2023-09-26 17:33:40 +0900

    feat: ChunkSize를 변경하는 db out port 메서드 선언

--- src/main/java/dandi/dandi/batchcommons/application/port/out/ChunkSizePersistencePort.java
+++ src/main/java/dandi/dandi/batchcommons/application/port/out/ChunkSizePersistencePort.java
@@ -5,4 +5,6 @@ import dandi.dandi.common.exception.NotFoundException;
 public interface ChunkSizePersistencePort {
 
     int findChunkSizeByName(String name) throws NotFoundException;
+
+    void updateChunkSizeByName(String name, int chunkSize) throws NotFoundException;
 }
