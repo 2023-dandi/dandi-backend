5eea678415fa47d1c005e395ee6a32a0ce0f7ce2
Chris <bjuuuu98@gmail.com>
2023-09-26 17:03:34 +0900

    feat: ChunkSize 엔티티 정의

---
+++ src/main/java/dandi/dandi/batchcommons/adapter/out/persistence/jpa/ChunkSizeJpaEntity.java
@@ -0,0 +1,28 @@
+package dandi.dandi.batchcommons.adapter.out.persistence.jpa;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "chunk_size")
+public class ChunkSizeJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chunk_size_id")
+    private Long id;
+
+    private String name;
+    private Integer value;
+
+    public ChunkSizeJpaEntity(String name, Integer value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public ChunkSizeJpaEntity() {
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
