1d5281602c3c29632aa515d7dc399cd47ce6bcf8
Chris <bjuuuu98@gmail.com>
2023-09-27 14:28:07 +0900

    feat: 문자열을 받아 문자열에 포함되어 있는 KmaResponseCode를 응답하는 메서드 구현

--- src/main/java/dandi/dandi/weather/adapter/out/kma/code/KmaResponseCode.java
+++ src/main/java/dandi/dandi/weather/adapter/out/kma/code/KmaResponseCode.java
@@ -1,62 +1,70 @@
 package dandi.dandi.weather.adapter.out.kma.code;
 
-import dandi.dandi.weather.application.port.out.WeatherRequestFatalException;
-import org.apache.http.protocol.HTTP;
-
 import java.util.Arrays;
+
+import javax.annotation.Nullable;
+
+import dandi.dandi.weather.application.port.out.WeatherRequestFatalException;
 
 public enum KmaResponseCode {
 
-    NORMAL_SERVICE("00"),
-    APPLICATION_ERROR("01"),
-    DB_ERROR("02"),
-    NO_DATA_ERROR("03"),
-    HTTP_ERROR("04"),
-    SERVICE_TIME_OUT("05"),
-    INVALID_REQUEST_PARAMETER_ERROR("10"),
-    NO_MANDATORY_REQUEST_PARAMETERS_ERROR("11"),
-    NO_OPENAPI_SERVICE_ERROR("12"),
-    SERVICE_ACCESS_DENIED_ERROR("20"),
-    TEMPORARILY_DISABLE_THE_SERVICE_KEY_ERROR("21"),
-    LIMITED_NUMBER_OF_SERVICE_REQUESTS_EXCEEDS_ERROR("22"),
-    SERVICE_KEY_IS_NOT_REGISTERED_ERROR("30"),
-    DEADLINE_HAS_EXPIRED_ERROR("31"),
-    UNREGISTERED_IP_ERROR("32"),
-    UNSIGNED_CALL_ERROR("33"),
-    UNKNOWN_ERROR("99"),
-    ;
+	NORMAL_SERVICE("00"),
+	APPLICATION_ERROR("01"),
+	DB_ERROR("02"),
+	NO_DATA_ERROR("03"),
+	HTTP_ERROR("04"),
+	SERVICE_TIME_OUT("05"),
+	INVALID_REQUEST_PARAMETER_ERROR("10"),
+	NO_MANDATORY_REQUEST_PARAMETERS_ERROR("11"),
+	NO_OPENAPI_SERVICE_ERROR("12"),
+	SERVICE_ACCESS_DENIED_ERROR("20"),
+	TEMPORARILY_DISABLE_THE_SERVICE_KEY_ERROR("21"),
+	LIMITED_NUMBER_OF_SERVICE_REQUESTS_EXCEEDS_ERROR("22"),
+	SERVICE_KEY_IS_NOT_REGISTERED_ERROR("30"),
+	DEADLINE_HAS_EXPIRED_ERROR("31"),
+	UNREGISTERED_IP_ERROR("32"),
+	UNSIGNED_CALL_ERROR("33"),
+	UNKNOWN_ERROR("99"),
+	;
 
-    private final String value;
+	private final String value;
 
-    KmaResponseCode(String value) {
-        this.value = value;
-    }
+	KmaResponseCode(String value) {
+		this.value = value;
+	}
 
-    public static KmaResponseCode from(String value) {
-        return Arrays.stream(values())
-                .filter(kmaResponseCode -> kmaResponseCode.value.equals(value))
-                .findFirst()
-                .orElseThrow(() -> new WeatherRequestFatalException("기상청 응답 코드를 변환할 수 없습니다."));
-    }
+	public static KmaResponseCode from(String value) {
+		return Arrays.stream(values())
+			.filter(kmaResponseCode -> kmaResponseCode.value.equals(value))
+			.findFirst()
+			.orElseThrow(() -> new WeatherRequestFatalException("기상청 응답 코드를 변환할 수 없습니다."));
+	}
 
-    public boolean isSuccessful() {
-        return this == NORMAL_SERVICE;
-    }
+	@Nullable
+	public static KmaResponseCode findByNameContainedInValueOrElseNull(String value) {
+		return Arrays.stream(values())
+			.filter(kmaResponseCode -> value.contains(kmaResponseCode.name()))
+			.findFirst()
+			.orElse(null);
+	}
 
-    public boolean isErrorAssociatedWithLocation() {
-        return this == NO_DATA_ERROR;
-    }
+	public boolean isSuccessful() {
+		return this == NORMAL_SERVICE;
+	}
 
-    public boolean isTemporaryExternalServerError() {
-        return this == SERVICE_TIME_OUT || this == DB_ERROR || this == HTTP_ERROR ||
-                this == SERVICE_ACCESS_DENIED_ERROR || this == TEMPORARILY_DISABLE_THE_SERVICE_KEY_ERROR ||
-                this == SERVICE_KEY_IS_NOT_REGISTERED_ERROR;
-    }
+	public boolean isErrorAssociatedWithLocation() {
+		return this == NO_DATA_ERROR;
+	}
 
-    public boolean isRetryable() {
-        return this == SERVICE_TIME_OUT || this == DB_ERROR || this == HTTP_ERROR ||
-                this == SERVICE_ACCESS_DENIED_ERROR || this == TEMPORARILY_DISABLE_THE_SERVICE_KEY_ERROR ||
-                this == SERVICE_KEY_IS_NOT_REGISTERED_ERROR;
+	public boolean isTemporaryExternalServerError() {
+		return this == SERVICE_TIME_OUT || this == DB_ERROR || this == HTTP_ERROR ||
+			this == SERVICE_ACCESS_DENIED_ERROR || this == TEMPORARILY_DISABLE_THE_SERVICE_KEY_ERROR ||
+			this == SERVICE_KEY_IS_NOT_REGISTERED_ERROR;
+	}
 
-    }
+	public boolean isRetryable() {
+		return this == SERVICE_TIME_OUT || this == DB_ERROR || this == HTTP_ERROR ||
+			this == SERVICE_ACCESS_DENIED_ERROR || this == TEMPORARILY_DISABLE_THE_SERVICE_KEY_ERROR ||
+			this == SERVICE_KEY_IS_NOT_REGISTERED_ERROR;
+	}
 }
