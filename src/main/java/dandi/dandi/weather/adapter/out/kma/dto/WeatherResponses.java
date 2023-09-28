5302293ffa5dba0ac77d3c4c7b3d6ab70eaacefe
Chris <bjuuuu98@gmail.com>
2023-09-28 15:04:24 +0900

    feat: 날씨 API 기상청 응답 구현체 정의

--- src/main/java/dandi/dandi/weather/adapter/out/kma/dto/WeatherResponses.java
+++ src/main/java/dandi/dandi/weather/adapter/out/kma/dto/WeatherResponses.java
@@ -1,6 +1,10 @@
 package dandi.dandi.weather.adapter.out.kma.dto;
 
-public class WeatherResponses {
+import java.util.List;
+
+import dandi.dandi.weather.adapter.out.kma.WeatherResponsesI;
+
+public class WeatherResponses implements WeatherResponsesI {
 
     private WeatherResponse response;
 
@@ -13,5 +17,32 @@ public class WeatherResponses {
 
     public WeatherResponse getResponse() {
         return response;
+    }
+
+    @Override
+    public boolean isSuccessful() {
+        return response.isSuccessful();
+    }
+
+    @Override
+    public boolean isRetryableError() {
+        return response.isRetryableError();
+    }
+
+    @Override
+    public boolean isNoDataLocationError() {
+        return response.isNoDataLocationError();
+    }
+
+    @Override
+    public List<WeatherItem> getWeatherItems() {
+        return response.getBody()
+            .getItems()
+            .getItem();
+    }
+
+    @Override
+    public String getResultMessage() {
+        return response.getResultMessage();
     }
 }
