21ff8e3404751a0e195b116ca610c63264ffd3ef
Chris <bjuuuu98@gmail.com>
2023-09-28 15:27:49 +0900

    test: ParameterizedTest 삭제

--- src/test/java/dandi/dandi/weather/adapter/out/kma/code/KmaResponseCodeTest.java
+++ src/test/java/dandi/dandi/weather/adapter/out/kma/code/KmaResponseCodeTest.java
@@ -5,18 +5,17 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class KmaResponseCodeTest {
 
 	@DisplayName("문자열을 받아 문자열에 포함되는 errorMessage를 가진 KmaResponseCode를 반환한다.")
-	@ParameterizedTest
-	@CsvSource({"abasdHTTP ROUTING ERRORadq12, HTTP_ROUTING_ERROR", "asdaNORMAL_SERVICEasdsa, NORMAL_SERVICE"})
-	void findByNameOrElseNull(String value, KmaResponseCode expected) {
+	@Test
+	void findByNameOrElseNull() {
+		String value = "asdaNORMAL_SERVICEasdsa";
+
 		KmaResponseCode actual = KmaResponseCode.findByNameContainedInValueOrElseNull(value);
 
-		assertThat(actual).isEqualTo(expected);
+		assertThat(actual).isEqualTo(NORMAL_SERVICE);
 	}
 
 	@DisplayName("문자열을 받아 문자열에 포함되는 errorMessage를 가진 KmaResponseCode가 없다면 null을 반환한다.")
