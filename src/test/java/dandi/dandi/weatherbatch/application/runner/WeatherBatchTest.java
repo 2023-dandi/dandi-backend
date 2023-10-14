30c5268ebc9b6de6f9679c2b7324640f5c49fcb6
Chris <bjuuuu98@gmail.com>
2023-10-14 19:43:55 +0900

    test: JobParameter로 날씨 API 스레드 개수 주입 테스트 코드 반영

--- src/test/java/dandi/dandi/weatherbatch/application/runner/WeatherBatchTest.java
+++ src/test/java/dandi/dandi/weatherbatch/application/runner/WeatherBatchTest.java
@@ -40,114 +40,122 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 @EnableJpaRepositories(basePackageClasses = {WeatherRepository.class})
 @EntityScan(basePackageClasses = {WeatherJpaEntity.class, WeatherLocationJpaEntity.class})
 @SpringBootTest(classes = {WeatherBatch.class, WeatherPersistenceAdapter.class, WeatherBatchTestConfig.class,
-	DatabaseCleaner.class})
+        WeatherBatchItemReaderConfig.class, WeatherBatchItemWriterConfig.class, DatabaseCleaner.class})
 class WeatherBatchTest {
 
-	@Autowired
-	private JobLauncherTestUtils jobLauncherTestUtils;
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
 
-	@Autowired
-	private WeatherRepository weatherRepository;
+    @Autowired
+    private WeatherRepository weatherRepository;
 
-	@Autowired
-	private EntityManagerFactory entityManagerFactory;
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
 
-	@Autowired
-	private DatabaseCleaner databaseCleaner;
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
 
-	@BeforeEach
-	void clearDatabase() {
-		databaseCleaner.truncate();
-		databaseCleaner.initializeAutoIncrement();
-	}
+    @BeforeEach
+    void clearDatabase() {
+        databaseCleaner.truncate();
+        databaseCleaner.initializeAutoIncrement();
+    }
 
-	@DisplayName("날씨 위치에 기반한 날씨 정보를 받아와 저장할 수 있다.")
-	@Test
-	void weatherBatchJob() throws Exception {
-		long weatherLocationId = 1L;
-		Weather firstWeather = new Weather.WeatherBuilder(LocalDateTime.now()).build();
-		Weather secondWeather = new Weather.WeatherBuilder(LocalDateTime.now()).build();
-		weatherRepository.saveAll(List.of(
-				WeatherJpaEntity.ofWeather(firstWeather, weatherLocationId),
-				WeatherJpaEntity.ofWeather(secondWeather, weatherLocationId)));
-		LocalDateTime dateTime = LocalDateTime.of(2023, 9, 13, 15, 0);
-		JobParameters jobParameters = new JobParameters(
-			Map.of("baseDateTime", new JobParameter(dateTime.toString()),
-					"backOffPeriod", new JobParameter(1L),
-					"chunkSize", new JobParameter(10L)));
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		entityManager.persist(new WeatherLocationJpaEntity(110, 100));
-		entityManager.getTransaction().commit();
+    @DisplayName("날씨 위치에 기반한 날씨 정보를 받아와 저장할 수 있다.")
+    @Test
+    void weatherBatchJob() throws Exception {
+        long weatherLocationId = 1L;
+        Weather firstWeather = new Weather.WeatherBuilder(LocalDateTime.now()).build();
+        Weather secondWeather = new Weather.WeatherBuilder(LocalDateTime.now()).build();
+        weatherRepository.saveAll(List.of(
+                WeatherJpaEntity.ofWeather(firstWeather, weatherLocationId),
+                WeatherJpaEntity.ofWeather(secondWeather, weatherLocationId)));
+        LocalDateTime dateTime = LocalDateTime.of(2023, 9, 13, 15, 0);
+        JobParameters jobParameters = new JobParameters(
+                Map.of("baseDateTime", new JobParameter(dateTime.toString()),
+                        "backOffPeriod", new JobParameter(1L),
+                        "chunkSize", new JobParameter(10L),
+                        "weatherApiThreadSize", new JobParameter(1L)
+                ));
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(new WeatherLocationJpaEntity(110, 100));
+        entityManager.getTransaction().commit();
 
-		JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
 
-		assertAll(
-			() -> assertThat(jobExecution.getExitStatus()).isEqualTo(ExitStatus.COMPLETED),
-			() -> assertThat(weatherRepository.findAll()).hasSize(24)
-		);
-	}
+        assertAll(
+                () -> assertThat(jobExecution.getExitStatus()).isEqualTo(ExitStatus.COMPLETED),
+                () -> assertThat(weatherRepository.findAll()).hasSize(24)
+        );
+    }
 
-	@DisplayName("날씨 위치에 기반한 날씨 정보를 받아오는 도중 WeatherRequestRetryableException이 발생하면 재시도 한다.")
-	@Test
-	void weatherBatchJob_RetryableException_Success() throws Exception {
-		LocalDateTime dateTime = LocalDateTime.of(2021, 9, 13, 15, 0);
-		JobParameters jobParameters = new JobParameters(
-				Map.of("baseDateTime", new JobParameter(dateTime.toString()),
-						"backOffPeriod", new JobParameter(1L),
-						"chunkSize", new JobParameter(10L)));
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		entityManager.persist(new WeatherLocationJpaEntity(110, 100));
-		entityManager.getTransaction().commit();
+    @DisplayName("날씨 위치에 기반한 날씨 정보를 받아오는 도중 WeatherRequestRetryableException이 발생하면 재시도 한다.")
+    @Test
+    void weatherBatchJob_RetryableException_Success() throws Exception {
+        LocalDateTime dateTime = LocalDateTime.of(2021, 9, 13, 15, 0);
+        JobParameters jobParameters = new JobParameters(
+                Map.of("baseDateTime", new JobParameter(dateTime.toString()),
+                        "backOffPeriod", new JobParameter(1L),
+                        "chunkSize", new JobParameter(10L),
+                        "weatherApiThreadSize", new JobParameter(1L)
+                ));
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(new WeatherLocationJpaEntity(110, 100));
+        entityManager.getTransaction().commit();
 
-		JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
 
-		assertAll(
-			() -> assertThat(jobExecution.getExitStatus()).isEqualTo(ExitStatus.COMPLETED),
-			() -> assertThat(weatherRepository.findAll()).hasSize(24)
-		);
-	}
+        assertAll(
+                () -> assertThat(jobExecution.getExitStatus()).isEqualTo(ExitStatus.COMPLETED),
+                () -> assertThat(weatherRepository.findAll()).hasSize(24)
+        );
+    }
 
-	@DisplayName("날씨 위치에 기반한 날씨 정보를 받아오는 도중 WeatherRequestRetryableException이 발생하고 1회의 재시도도 실패하면 재시도 하지 않는다.")
-	@Test
-	void weatherBatchJob_RetryableException_Failed() throws Exception {
-		LocalDateTime dateTime = LocalDateTime.of(2022, 9, 13, 15, 0);
-		JobParameters jobParameters = new JobParameters(
-				Map.of("baseDateTime", new JobParameter(dateTime.toString()),
-						"backOffPeriod", new JobParameter(1L),
-						"chunkSize", new JobParameter(10L)));
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		entityManager.persist(new WeatherLocationJpaEntity(110, 100));
-		entityManager.getTransaction().commit();
+    @DisplayName("날씨 위치에 기반한 날씨 정보를 받아오는 도중 WeatherRequestRetryableException이 발생하고 1회의 재시도도 실패하면 재시도 하지 않는다.")
+    @Test
+    void weatherBatchJob_RetryableException_Failed() throws Exception {
+        LocalDateTime dateTime = LocalDateTime.of(2022, 9, 13, 15, 0);
+        JobParameters jobParameters = new JobParameters(
+                Map.of("baseDateTime", new JobParameter(dateTime.toString()),
+                        "backOffPeriod", new JobParameter(1L),
+                        "chunkSize", new JobParameter(10L),
+                        "weatherApiThreadSize", new JobParameter(1L)
+                ));
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(new WeatherLocationJpaEntity(110, 100));
+        entityManager.getTransaction().commit();
 
-		JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
 
-		assertAll(
-			() -> assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo(ExitStatus.FAILED.getExitCode()),
-			() -> assertThat(weatherRepository.findAll()).hasSize(0)
-		);
-	}
+        assertAll(
+                () -> assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo(ExitStatus.FAILED.getExitCode()),
+                () -> assertThat(weatherRepository.findAll()).hasSize(0)
+        );
+    }
 
-	@DisplayName("날씨 위치에 기반한 날씨 정보를 받아오는 도중 WeatherRequestFatalException이 발생하고 실패한다.")
-	@Test
-	void weatherBatchJob_FatalException_Failed() throws Exception {
-		LocalDateTime dateTime = LocalDateTime.of(2020, 9, 13, 15, 0);
-		JobParameters jobParameters = new JobParameters(
-				Map.of("baseDateTime", new JobParameter(dateTime.toString()),
-						"backOffPeriod", new JobParameter(1L),
-						"chunkSize", new JobParameter(10L)));
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		entityManager.persist(new WeatherLocationJpaEntity(110, 100));
-		entityManager.getTransaction().commit();
+    @DisplayName("날씨 위치에 기반한 날씨 정보를 받아오는 도중 WeatherRequestFatalException이 발생하고 실패한다.")
+    @Test
+    void weatherBatchJob_FatalException_Failed() throws Exception {
+        LocalDateTime dateTime = LocalDateTime.of(2020, 9, 13, 15, 0);
+        JobParameters jobParameters = new JobParameters(
+                Map.of("baseDateTime", new JobParameter(dateTime.toString()),
+                        "backOffPeriod", new JobParameter(1L),
+                        "chunkSize", new JobParameter(10L),
+                        "weatherApiThreadSize", new JobParameter(1L)
+                ));
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(new WeatherLocationJpaEntity(110, 100));
+        entityManager.getTransaction().commit();
 
-		JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
 
-		assertAll(
-			() -> assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo(ExitStatus.FAILED.getExitCode()),
-			() -> assertThat(weatherRepository.findAll()).hasSize(0)
-		);
-	}
+        assertAll(
+                () -> assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo(ExitStatus.FAILED.getExitCode()),
+                () -> assertThat(weatherRepository.findAll()).hasSize(0)
+        );
+    }
 }
