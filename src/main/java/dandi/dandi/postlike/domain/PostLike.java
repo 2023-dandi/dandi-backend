26dee501455dd6f3d40ca2e215f4fe1e7f4593e8
Chris <bjuuuu98@gmail.com>
2023-10-17 19:50:32 +0900

    refactor: PostLike 도메인에 id 필드 삭제

--- src/main/java/dandi/dandi/postlike/domain/PostLike.java
+++ src/main/java/dandi/dandi/postlike/domain/PostLike.java
@@ -4,22 +4,16 @@ import java.util.Objects;
 
 public class PostLike {
 
-    private final Long id;
     private final Long memberId;
     private final Long postId;
 
-    public PostLike(Long id, Long memberId, Long postId) {
-        this.id = id;
+    public PostLike(Long memberId, Long postId) {
         this.memberId = memberId;
         this.postId = postId;
     }
 
     public static PostLike initial(Long memberId, Long postId) {
-        return new PostLike(null, memberId, postId);
-    }
-
-    public Long getId() {
-        return id;
+        return new PostLike(memberId, postId);
     }
 
     public Long getMemberId() {
@@ -32,19 +26,14 @@ public class PostLike {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof PostLike)) {
-            return false;
-        }
+        if (this == o) return true;
+        if (!(o instanceof PostLike)) return false;
         PostLike postLike = (PostLike) o;
-        return Objects.equals(id, postLike.id) && Objects.equals(memberId, postLike.memberId)
-                && Objects.equals(postId, postLike.postId);
+        return Objects.equals(memberId, postLike.memberId) && Objects.equals(postId, postLike.postId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, memberId, postId);
+        return Objects.hash(memberId, postId);
     }
 }
